4567faa6017a68993080eea8439d60eb1c4b7db9
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2016-04-17 09:54:05 -0400

    loolwsd: childConnected event added to UnitWSD
    Change-Id: I3d19465229b434d07f50290f08462e9c9c66a6ca

--- loolwsd/Unit.hpp
+++ loolwsd/Unit.hpp
@@ -131,7 +131,8 @@ public:
                      Poco::Net::HTTPServerResponse& /* response */)
         { return false; }
 
-
+    // ---------------- WSD events ----------------
+    virtual void onChildConnected(const int /* pid */, const std::string& /* sessionId */) {}
 };
 
 /// Derive your Kit unit test / hooks from me.
