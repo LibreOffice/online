d446c27ebd586709dd1e440f8ef240eeb54641de
Marco Cecchetti <mrcekets@gmail.com>
2016-10-17 09:51:20 +0000

    Revert "loleaflet: handle EMPTY invalid tiles msg with part in the payload"
    This reverts commit 78e707393e2f630cb2c9ca7aa5b14d4947f3b9ed.
    
    Change-Id: Ia5c9e080aeabdd595f0078e0cf85a7c17d6b78ba

--- loleaflet/src/layer/tile/TileLayer.js
+++ loleaflet/src/layer/tile/TileLayer.js
@@ -304,16 +304,15 @@ L.TileLayer = L.GridLayer.extend({
 		else if (textMsg.startsWith('invalidatecursor:')) {
 			this._onInvalidateCursorMsg(textMsg);
 		}
-		else if (textMsg.startsWith('invalidatetiles:') && !textMsg.startsWith('EMPTY')) {
+		else if (textMsg.startsWith('invalidatetiles:') && !textMsg.match('EMPTY')) {
 			this._onInvalidateTilesMsg(textMsg);
 		}
-		else if (textMsg.startsWith('invalidatetiles:') && textMsg.startsWith('EMPTY')) {
+		else if (textMsg.startsWith('invalidatetiles:') && textMsg.match('EMPTY')) {
 			var msg = 'invalidatetiles: ';
 			if (this._docType === 'text') {
 				msg += 'part=0 ';
 			} else {
-				var partNumber = parseInt(textMsg.substring(6));
-				msg += 'part=' + (isNaN(partNumber) ? this._selectedPart : partNumber) + ' ';
+				msg += 'part=' + this._selectedPart + ' ';
 			}
 			msg += 'x=0 y=0 ';
 			msg += 'width=' + this._docWidthTwips + ' ';
