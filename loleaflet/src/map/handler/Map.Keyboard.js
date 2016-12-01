LibreOffice/online

commit 47699cd9084e4562dd7983f6c4d48cef74b6ba0b

tdf#98484: Handle spaces in chrome when IME is enabled
When IME is enabled and user presses the space button, firefox
sends 'compositionend' event with data = ' '. However, chrome
instead fires 'textInput'
event. Lets listen for 'textInput' event too and treat it like
'compositionend' event when data = ' '.

Change-Id: Icbebdf2e89f608f790e6ce68c49f474364e7d5ab

loleaflet/src/map/handler/Map.Keyboard.js
@@ -171,13 +171,13 @@ L.Map.Keyboard = L.Handler.extend({
 
 		this._map.on('mousedown', this._onMouseDown, this);
 		this._map.on('keydown keyup keypress', this._onKeyDown, this);
-		this._map.on('compositionend', this._onKeyDown, this);
+		this._map.on('compositionstart compositionupdate compositionend textInput', this._onKeyDown, this);
 	},
 
 	removeHooks: function () {
 		this._map.off('mousedown', this._onMouseDown, this);
 		this._map.off('keydown keyup keypress', this._onKeyDown, this);
-		this._map.off('compositionend', this._onKeyDown, this);
+		this._map.off('compositionstart compositionupdate compositionend textInput', this._onKeyDown, this);
 	},
 
 	_setPanOffset: function (pan) {
@@ -285,6 +285,14 @@ L.Map.Keyboard = L.Handler.extend({
 
 		var charCode = e.originalEvent.charCode;
 		var keyCode = e.originalEvent.keyCode;
+
+		// Hack for making space work in chrome when IME is enabled
+		// Chrome doesn't fire compositionend event when IME is enabled and user presses <space>.
+		// However, it sends 'textInput' event in such a case. treat it like 'compositionend' events
+		if (e.type === 'textInput' && e.originalEvent.data === ' ') {
+			e.type = 'compositionend';
+		}
+
 		if (e.type === 'compositionend') {
 			var compCharCodes = [];
 			for (var i = 0; i < e.originalEvent.data.length; i++) {
@@ -357,6 +365,7 @@ L.Map.Keyboard = L.Handler.extend({
 				map.setZoom(map.getZoom() + (e.shiftKey ? 3 : 1) * this._zoomKeys[key]);
 			}
 		}
+
 		L.DomEvent.stopPropagation(e.originalEvent);
 	},
 

loleaflet/src/map/Map.js
@@ -721,7 +721,7 @@ L.Map = L.Evented.extend({
 		L.DomEvent[onOff](this._container, 'click dblclick mousedown mouseup ' +
 			'mouseover mouseout mousemove contextmenu dragover drop ' +
 			'keydown keypress keyup trplclick qdrplclick', this._handleDOMEvent, this);
-		L.DomEvent[onOff](this._textArea, 'copy cut paste keydown keypress keyup compositionend', this._handleDOMEvent, this);
+		L.DomEvent[onOff](this._textArea, 'copy cut paste keydown keypress keyup compositionstart compositionupdate compositionend textInput', this._handleDOMEvent, this);
 
 		if (this.options.trackResize && this._resizeDetector.contentWindow) {
 			L.DomEvent[onOff](this._resizeDetector.contentWindow, 'resize', this._onResize, this);
@@ -941,7 +941,7 @@ L.Map = L.Evented.extend({
 		};
 		if (e.type !== 'keypress' && e.type !== 'keyup' && e.type !== 'keydown' &&
 			e.type !== 'copy' && e.type !== 'cut' && e.type !== 'paste' &&
-			e.type !== 'compositionend') {
+		    e.type !== 'compositionstart' && e.type !== 'compositionupdate' && e.type !== 'compositionend' && e.type !== 'textInput') {
 			data.containerPoint = target instanceof L.Marker ?
 					this.latLngToContainerPoint(target.getLatLng()) : this.mouseEventToContainerPoint(e);
 			data.layerPoint = this.containerPointToLayerPoint(data.containerPoint);

commit 05a8047473566e3d25e191a11f0878dd5bc8d1c7

tdf#98484: Fix double-space in chrome when IME enabled
We regressed on chrome with
47699cd9084e4562dd7983f6c4d48cef74b6ba0b as 'textInput' event
clubbed with 'keypress' event started emitting double-spaces.

Tweak the hack to keep IME working on chrome while not regressing
when it is not enabled.

Change-Id: I0901724bf2db1794078fef3eb7283663b6f6a13b

loleaflet/src/map/handler/Map.Keyboard.js
@@ -235,6 +235,7 @@ L.Map.Keyboard = L.Handler.extend({
 		var alt = e.originalEvent.altKey ? this.keyModifier.alt : 0;
 		var cmd = e.originalEvent.metaKey ? this.keyModifier.ctrl : 0;
 		var location = e.originalEvent.location;
+		this._keyHandled = this._keyHandled || false;
 		this.modifier = shift | ctrl | alt | cmd;
 
 		// On Windows, pressing AltGr = Alt + Ctrl
@@ -286,13 +287,6 @@ L.Map.Keyboard = L.Handler.extend({
 		var charCode = e.originalEvent.charCode;
 		var keyCode = e.originalEvent.keyCode;
 
-		// Hack for making space work in chrome when IME is enabled
-		// Chrome doesn't fire compositionend event when IME is enabled and user presses <space>.
-		// However, it sends 'textInput' event in such a case. treat it like 'compositionend' events
-		if (e.type === 'textInput' && e.originalEvent.data === ' ') {
-			e.type = 'compositionend';
-		}
-
 		if (e.type === 'compositionend') {
 			var compCharCodes = [];
 			for (var i = 0; i < e.originalEvent.data.length; i++) {
@@ -313,8 +307,13 @@ L.Map.Keyboard = L.Handler.extend({
 		if (this._map._permission === 'edit') {
 			docLayer._resetPreFetching();
 
-			if (e.type === 'keydown' && this.handleOnKeyDown[keyCode] && charCode === 0) {
-				docLayer._postKeyboardEvent('input', charCode, unoKeyCode);
+			if (e.type === 'keydown') {
+				this._keyHandled = false;
+				this._bufferedTextInputEvent = null;
+
+				if (this.handleOnKeyDown[keyCode] && charCode === 0) {
+					docLayer._postKeyboardEvent('input', charCode, unoKeyCode);
+				}
 			}
 			else if ((e.type === 'keypress' || e.type === 'compositionend') &&
 				(!this.handleOnKeyDown[keyCode] || charCode !== 0)) {
@@ -334,9 +333,28 @@ L.Map.Keyboard = L.Handler.extend({
 				} else {
 					docLayer._postKeyboardEvent('input', charCode, unoKeyCode);
 				}
+
+				this._keyHandled = true;
+			}
+			else if (e.type === 'textInput' && !this._keyHandled) {
+				// Store the textInput event
+				this._bufferedTextInputEvent = e;
 			}
 			else if (e.type === 'keyup') {
+				// Hack for making space work in chrome when IME is enabled
+				// Chrome doesn't fire compositionend event or keypress when
+				// IME is enabled *and* user presses <space>.
+				// However, it sends 'textInput' event in such a case.
+				// Use the buffered textInput event if its the space key and has not been
+				// handled already by 'keypress' or 'compositionend' events above
+				if (!this._keyHandled && this._bufferedTextInputEvent && e.originalEvent.key === this._bufferedTextInputEvent.originalEvent.data) {
+					charCode = e.originalEvent.keyCode;
+					docLayer._postKeyboardEvent('input', charCode, 0);
+				}
 				docLayer._postKeyboardEvent('up', charCode, unoKeyCode);
+
+				this._keyHandled = true;
+				this._bufferedTextInputEvent = null;
 			}
 			if (keyCode === 9) {
 				// tab would change focus to other DOM elements