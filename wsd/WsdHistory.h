4cbe7dfc7d4a1e23c6b87c8e4278660cd616007f
Marco Viscido <marcoviscido@gmail.com>
2017-03-20 19:09:48 +0100

    redundant classes removed
    Change-Id: I78ade38611165ee6c1c9353d34e755ca194b180e

--- wsd/WsdHistory.h
+++ wsd/WsdHistory.h
@@ -25,109 +25,85 @@ class Document;
 namespace WsdStats
 {
 
-    class MsgEvent
-    {
-    public:
-        MsgEvent ();
-        MsgEvent(const std::string& action, const std::string& pid, const std::string& filename, const std::string& sid);
-        ~MsgEvent ();
-        
-        void set_action (std::string new_var)
-        {
-            _action = new_var;
-        }
+//    class MsgEvent
+//    {
+//    public:
+//        MsgEvent ();
+//        MsgEvent(const std::string& action, const std::string& pid, const std::string& filename, const std::string& sid);
+//        ~MsgEvent ();
+//
+//        void set_action (std::string new_var)
+//        {
+//            _action = new_var;
+//        }
+//
+//        std::string get_action ()
+//        {
+//            return _action;
+//        }
+//
+//        void set_pid (unsigned long new_var)
+//        {
+//            _pid = new_var;
+//        }
+//
+//        std::string get_pid ()
+//        {
+//          return _pid;
+//        }
+//
+//        void set_filename (std::string new_var)
+//        {
+//            _filename = new_var;
+//        }
+//
+//        std::string get_filename ()
+//        {
+//          return _filename;
+//        }
+//
+//        void set_sessionId (unsigned long new_var)
+//        {
+//            _sessionId = new_var;
+//        }
+//
+//        std::string get_sessionId ()
+//        {
+//            return _sessionId;
+//        }
+//
+//    private:
+//        std::string _action;
+//        std::string _pid;
+//        std::string _filename;
+//        std::string _sessionId;
+//    };
 
-        std::string get_action ()
-        {
-            return _action;
-        }
+    //typedef typename std::shared_ptr<MsgEvent> MsgEventPtr;
+    //typedef typename std::map<long int, MsgEventPtr> EventsMapType;
+    //typedef typename std::shared_ptr<EventsMapType> EventsMapPtr;
 
-        void set_pid (unsigned long new_var)
-        {
-            _pid = new_var;
-        }
-
-        std::string get_pid ()
-        {
-          return _pid;
-        }
-
-        void set_filename (std::string new_var)
-        {
-            _filename = new_var;
-        }
-
-        std::string get_filename ()
-        {
-          return _filename;
-        }
-
-        void set_sessionId (unsigned long new_var)
-        {
-            _sessionId = new_var;
-        }
-
-        std::string get_sessionId ()
-        {
-            return _sessionId;
-        }
-
-    private:
-        std::string _action;
-        std::string _pid;
-        std::string _filename;
-        std::string _sessionId;
-    };
-
-    class MsgDocument
-    {
-        friend std::ostream& WsdStats::operator<<(std::ostream& oss, WsdStats::MsgDocument const& v)
-        {
-            oss << v._pid << "\t" << v._filename << "\t" << v._numberOfViews << "\t" << v._memoryConsumed << "\t" << v._elapsedTime << "\t" << v._idleTime;
-            return oss;
-        }
-
-    public:
-        MsgDocument ();
-        MsgDocument(std::string dockey, std::string pid, std::string fn, std::string numViews, std::string mem, std::string et, std::string it);
-        ~MsgDocument ();
-        std::string to_string();
-        bool operator <( const WsdStats::MsgDocument &rhs ) const { return ( _pid < rhs._pid ); }
-
-    private:
-        std::string _docKey;
-        std::string _pid;
-        std::string _filename;
-        std::string _numberOfViews;
-        std::string _memoryConsumed;
-        std::string _elapsedTime;
-        std::string _idleTime;
-    };
+    typedef typename std::shared_ptr<Document> DocumentPtr;
     
-    typedef typename std::shared_ptr<MsgEvent> MsgEventPtr;
-    typedef typename std::map<long int, MsgEventPtr> EventsMapType;
-    typedef typename std::shared_ptr<EventsMapType> EventsMapPtr;
+    typedef typename std::map<std::time_t,std::string> TimeString_MapType;
 
-    typedef typename std::shared_ptr<MsgDocument> MsgDocumentPtr;
-    typedef typename std::set<MsgDocumentPtr> DocumentMessSetType;
-    typedef typename std::shared_ptr<DocumentMessSetType> DocumentsSetPtr;
-
-    typedef typename std::list<MsgDocumentPtr> SnapshotListType;
-    typedef typename std::shared_ptr<SnapshotListType> SnapshotListPtr;
-    typedef typename std::map<long int, SnapshotListPtr> SnapshotsMapType;
-    typedef typename std::shared_ptr<SnapshotsMapType> SnapshotsMapPtr;
+    typedef typename std::shared_ptr<TimeString_MapType> DocSnapshots_MapPtr;    
+    typedef typename std::shared_ptr<TimeString_MapType> Events_MapPtr;
+    
+    typedef typename std::map<std::string,DocSnapshots_MapPtr> AllSnapshots_MapType;
+    typedef typename std::shared_ptr<AllSnapshots_MapType> AllSnapshots_MapPtr;
 
     class WsdHistory
-    {        
+    {
     public:
         WsdHistory();
         ~WsdHistory();
-        void collect(const std::string& message, const std::map<std::string,Document>& documents);
+        //void collect(const std::string& message, const std::map<std::string,Document>& documents);
+        std::time_t collectEvent(const std::string& message);
 
-    public: // TODO: must be private      
-      EventsMapPtr _events;
-      DocumentsSetPtr _messages; // will be an unordered_set!!
-      SnapshotsMapPtr _documents;
+    public: // TODO: must be private
+      Events_MapPtr _events;
+      AllSnapshots_MapPtr _allSnapshotsMap;
     };
 
 }
