da30cebebf3ab9b3cb4d0ea172e1fa92ff7b97b3
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2017-04-09 16:33:28 -0400

    wsd: don't call virtuals in dtors
    Change-Id: I2490e2f63dc20cf6b3fa0be45341b041e3ccb1bf
    Reviewed-on: https://gerrit.libreoffice.org/36321
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>
    (cherry picked from commit 2ad3cd4de169625f89911953f7311f157b6ee26d)

--- net/Socket.hpp
+++ net/Socket.hpp
@@ -644,7 +644,7 @@ public:
         if (!_shutdownSignalled)
         {
             _shutdownSignalled = true;
-            closeConnection();
+            StreamSocket::closeConnection();
         }
     }
 
