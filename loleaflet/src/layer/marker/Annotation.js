d7b4f74a0c0c45ac6bf491e10581fef0b7f374ab
Pranav Kant <pranavk@collabora.co.uk>
2017-04-24 19:59:16 +0530

    loleaflet: new dependency - autolinker - link what can be linked
    ... in annotations. Emails, phone numbers, email address and what not.
    We are not sending the generated markup, eg: the html <a> tags back to
    LO core, only the representation in the UI is linkified.
    
    Autolinker license: MIT
    https://www.npmjs.com/package/autolinker
    
    (cherry picked from commit 3a10c480139d0c499853df01f6bf5ae347656e7c)
    and
    (cherry picked from commit 7486f44ee1beada574906ae31a551b8c45af370e)
    
    Change-Id: I0fa03cbd25d80121bb047691cf3969caee65a6ef

--- loleaflet/src/layer/marker/Annotation.js
+++ loleaflet/src/layer/marker/Annotation.js
@@ -212,20 +212,21 @@ L.Annotation = L.Layer.extend({
 
 	_onCancelClick: function (e) {
 		L.DomEvent.stopPropagation(e);
-		this._nodeModifyText.value = this._contentText.innerHTML;
+		this._nodeModifyText.value = this._contentText.origText;
 		this.show();
 		this._map.fire('AnnotationCancel', {annotation: this});
 	},
 
 	_onSaveComment: function (e) {
 		L.DomEvent.stopPropagation(e);
-		this._data.text = this._contentText.innerHTML = this._nodeModifyText.value;
+		this._data.text = this._nodeModifyText.value;
+		this._updateContent();
 		this.show();
 		this._map.fire('AnnotationSave', {annotation: this});
 	},
 
 	_onLostFocus: function (e) {
-		if (this._contentText.innerHTML !== this._nodeModifyText.value) {
+		if (this._contentText.origText !== this._nodeModifyText.value) {
 			this._onSaveComment(e);
 		}
 	},
@@ -275,7 +276,10 @@ L.Annotation = L.Layer.extend({
 		if (!(this._data.dateTime instanceof Date)) {
 			this._data.dateTime = new Date(this._data.dateTime.replace(/,.*/, 'Z'));
 		}
-		this._contentText.innerHTML = this._nodeModifyText.innerHTML = this._data.text;
+		var linkedText = Autolinker.link(this._data.text);
+		this._contentText.innerHTML = linkedText;
+		// Original unlinked text
+		this._contentText.origText = this._nodeModifyText.innerHTML = this._data.text;
 		this._contentAuthor.innerHTML = this._data.author;
 		this._contentDate.innerHTML = this._data.dateTime.toDateString();
 		if (this._data.trackchange) {
