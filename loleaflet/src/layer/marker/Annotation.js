LibreOffice/online

commit 07607863925cf4fc2dbc91685d8f430f849c5535

loleaflet: Commit reply comment when focus lost
Change-Id: I0a47c6b74afed31264aec06ab654063fb5beaca8

loleaflet/src/layer/marker/Annotation.js
@@ -191,6 +191,7 @@ L.Annotation = L.Layer.extend({
 
 		buttons = L.DomUtil.create(tagDiv, empty, this._nodeModify);
 		L.DomEvent.on(this._nodeModifyText, 'blur', this._onLostFocus, this);
+		L.DomEvent.on(this._nodeReplyText, 'blur', this._onLostFocusReply, this);
 		this._createButton(buttons, _('Save'), this._onSaveComment);
 		this._createButton(buttons, cancel, this._onCancelClick);
 		buttons = L.DomUtil.create(tagDiv, empty, this._nodeReply);
@@ -228,6 +229,12 @@ L.Annotation = L.Layer.extend({
 	_onLostFocus: function (e) {
 		if (this._contentText.origText !== this._nodeModifyText.value) {
 			this._onSaveComment(e);
+		}
+	},
+
+	_onLostFocusReply: function(e) {
+		if (this._nodeReplyText.value !== '') {
+			this._onReplyClick(e);
 		}
 	},
 

commit 65b238aaa8b5b42fa217fe3158bbd0bc12293f4e

loleaflet: don't add comment reply when hitting Cancel
Regression from 07607863925cf4fc2dbc91685d8f430f849c5535

Change-Id: I49a5224ff70de65623496cfcba538acb6a8086f8

loleaflet/src/layer/marker/Annotation.js
@@ -217,6 +217,7 @@ L.Annotation = L.Layer.extend({
 	_onCancelClick: function (e) {
 		L.DomEvent.stopPropagation(e);
 		this._nodeModifyText.value = this._contentText.origText;
+		this._nodeReplyText.value = null;
 		this.show();
 		this._map.fire('AnnotationCancel', {annotation: this});
 	},