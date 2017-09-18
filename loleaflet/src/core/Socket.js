LibreOffice/online

commit 7c1e1120c63661d84a5696c5b10a961477228e19

loleaflet: ensure initialize map components before the document is loaded
Change-Id: I3b6219926f0f9d0306ef25bc5f9a9145410bac74

loleaflet/src/core/Socket.js
@@ -18,6 +18,11 @@ L.Socket = L.Class.extend({
 	initialize: function (map) {
 		console.debug('socket.initialize:');
 		this._map = map;
+		this._msgQueue = [];
+	},
+
+	connect: function() {
+		var map = this._map;
 		try {
 			if (map.options.permission) {
 				map.options.docParams['permission'] = map.options.permission;
@@ -43,7 +48,6 @@ L.Socket = L.Class.extend({
 			this._accessTokenExpireTimeout = setTimeout(L.bind(this._sessionExpiredWarning, this),
 			                                            parseInt(map.options.docParams.access_token_ttl) - Date.now() - tokenExpiryWarning);
 		}
-		this._msgQueue = [];
 	},
 
 	_sessionExpiredWarning: function() {

loleaflet/src/map/Map.js
@@ -169,6 +169,10 @@ L.Map = L.Evented.extend({
 		}, this);
 	},
 
+	loadDocument: function() {
+		this._socket.connect();
+	},
+
 	// public methods that modify map state
 
 	getViewId: function (username) {

loleaflet/main.js
@@ -132,6 +132,7 @@ map.addControl(L.control.columnHeader());
 map.addControl(L.control.rowHeader());
 map.addControl(L.control.contextMenu());
 map.addControl(L.control.menubar());
+map.loadDocument();
 
 window.addEventListener('beforeunload', function () {
 	if (global.map && global.map._socket) {

commit b96b017fe91cfc2b7f44ed94a3eaf567283d1c25

Don't forget reconnecting the socket in these cases
This regressed with 7c1e1120c63661d84a5696c5b10a961477228e19

Calling .initialize() on socket object is not enough anymore after above
mentioned commit; it doesn't re-establish the socket connection as
was the intent earlier in following cases.

Change-Id: Icd0a87a875eb275ed3c914474cdc502b460e2d16

loleaflet/src/core/Socket.js
@@ -91,7 +91,7 @@ L.Socket = L.Class.extend({
 
 		var socketState = this.socket.readyState;
 		if (socketState === 2 || socketState === 3) {
-			this.initialize(this._map);
+			this._map.loadDocument();
 		}
 
 		if (socketState === 1) {
@@ -297,7 +297,7 @@ L.Socket = L.Class.extend({
 					}
 
 					try {
-						socket.initialize(map);
+						map.loadDocument(map);
 					} catch (error) {
 					}
 				}, timeoutMs);
@@ -514,10 +514,10 @@ L.Socket = L.Class.extend({
 					callback: L.bind(function(data) {
 						if (data) {
 							this._map._docPassword = data.password;
-							this.initialize(this._map);
+							this._map.loadDocument();
 						} else if (passwordType === 'to-modify') {
 							this._map._docPassword = '';
-							this.initialize(this._map);
+							this._map.loadDocument();
 						} else {
 							this._map.hideBusy();
 						}

loleaflet/src/map/Map.js
@@ -845,7 +845,7 @@ L.Map = L.Evented.extend({
 					return vex.close(id);
 				}
 			} else {
-				this._socket.initialize(this);
+				this.loadDocument();
 			}
 		}
 