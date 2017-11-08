8bc8c63597846a6aee405fe39adf752ca7c62e98
Aron Budea <aron.budea@collabora.com>
2017-11-08 17:39:18 +0100

    wsd: increase preinit timeout 5-fold
    Change-Id: I08ab74498ef9396edba5311a7e31b75c5932bbd6

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -2720,7 +2720,7 @@ int LOOLWSD::innerMain()
         }
         else
         {
-            const auto timeoutMs = CHILD_TIMEOUT_MS * (LOOLWSD::NoCapsForKit ? 150 : 10);
+            const auto timeoutMs = 5 * CHILD_TIMEOUT_MS * (LOOLWSD::NoCapsForKit ? 150 : 10);
             const auto timeout = std::chrono::milliseconds(timeoutMs);
             LOG_TRC("Waiting for a new child for a max of " << timeoutMs << " ms.");
             if (!NewChildrenCV.wait_for(lock, timeout, []() { return !NewChildren.empty(); }))
