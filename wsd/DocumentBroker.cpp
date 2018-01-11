5581a1bdac5f40cda6d003f737c2c07f10f523ab
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2018-01-11 01:29:08 -0500

    wsd: save documents before recycling
    Change-Id: I24afc5c1bceca272f23a129609f73fc5a8d3caf2
    Reviewed-on: https://gerrit.libreoffice.org/47739
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>
    (cherry picked from commit 5f175c5120fa719563dca6eea625f351bdeada34)

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -269,6 +269,7 @@ void DocumentBroker::pollThread()
 
         if (ShutdownRequestFlag)
         {
+            autoSave(true);
             _closeReason = "recycling";
             _stop = true;
         }
@@ -295,7 +296,7 @@ void DocumentBroker::pollThread()
 
     LOG_INF("Finished polling doc [" << _docKey << "]. stop: " << _stop << ", continuePolling: " <<
             _poll->continuePolling() << ", ShutdownRequestFlag: " << ShutdownRequestFlag <<
-            ", TerminationFlag: " << TerminationFlag << ".");
+            ", TerminationFlag: " << TerminationFlag << ", closeReason: " << _closeReason << ". Flushing socket.");
 
     if (_isModified)
     {
@@ -316,6 +317,10 @@ void DocumentBroker::pollThread()
 
         _poll->poll(std::min(flushTimeoutMs - elapsedMs, POLL_TIMEOUT_MS / 5));
     }
+
+    LOG_INF("Finished flushing socket for doc [" << _docKey << "]. stop: " << _stop << ", continuePolling: " <<
+            _poll->continuePolling() << ", ShutdownRequestFlag: " << ShutdownRequestFlag <<
+            ", TerminationFlag: " << TerminationFlag << ". Terminating child with reason: [" << _closeReason << "].");
 
     // Terminate properly while we can.
     terminateChild(_closeReason);
@@ -866,7 +871,8 @@ bool DocumentBroker::autoSave(const bool force)
     if (force)
     {
         LOG_TRC("Sending forced save command for [" << _docKey << "].");
-        sent = sendUnoSave(savingSessionId);
+        // Don't terminate editing as this can be invoked by the admin OOM, but otherwise force saving anyway.
+        sent = sendUnoSave(savingSessionId, /*dontTerminateEdit=*/ true, /*dontSaveIfUnmodified=*/ false, /*isAutosave=*/ false);
     }
     else if (_isModified)
     {
