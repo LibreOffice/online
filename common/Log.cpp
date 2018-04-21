25d16d73067ef72c11df4364c79a77ee4847f508
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2018-04-21 13:49:35 -0400

    wsd: log proper ISO date in YYYY-MM-DD format
    Change-Id: I50f70dd2b3fc45536ccdf1c9a749ef1dab0a31ff

--- common/Log.cpp
+++ common/Log.cpp
@@ -111,10 +111,10 @@ namespace Log
             threadName = Util::getThreadName();
         }
         Poco::DateTime time;
-        snprintf(buffer, 1023, "%s-%.05lu %.2u/%.2u %.2u:%.2u:%.2u.%.6u [ %s ] %s  ",
+        snprintf(buffer, 1023, "%s-%.05lu %.4u-%.2u-%.2u %.2u:%.2u:%.2u.%.6u [ %s ] %s  ",
                     (Source.inited ? Source.id.c_str() : "<shutdown>"),
                     osTid,
-                    time.day(), time.month(),
+                    time.year(), time.month(), time.day(),
                     time.hour(), time.minute(), time.second(),
                     time.millisecond() * 1000 + time.microsecond(),
                     threadName, level);
