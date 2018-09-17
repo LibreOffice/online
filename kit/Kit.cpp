4fbb0f3b0d2cdecac044084d347d87b170af3d78
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2018-09-17 06:31:36 -0400

    wsd: leaflet: track multi-selection
    Change-Id: I17c092e950fb4d7a0cb4129c537d60a8e5edd06a
    Reviewed-on: https://gerrit.libreoffice.org/69636
    Reviewed-by: Ashod Nakashian <ashnakash@gmail.com>
    Tested-by: Ashod Nakashian <ashnakash@gmail.com>

--- kit/Kit.cpp
+++ kit/Kit.cpp
@@ -1589,6 +1589,15 @@ private:
         return _editorId;
     }
 
+    /// Notify all views with the given message
+    bool notifyAll(const std::string& msg) override
+    {
+        Util::assertIsLocked(_documentMutex);
+
+        // Broadcast updated viewinfo to all clients.
+        return sendTextFrame("client-all " + msg);
+    }
+
     /// Notify all views of viewId and their associated usernames
     void notifyViewInfo() override
     {
@@ -1636,10 +1645,9 @@ private:
 
         oss.seekp(-1, std::ios_base::cur); // Remove last comma.
         oss << "]";
-        const std::string msg = oss.str();
 
         // Broadcast updated viewinfo to all clients.
-        sendTextFrame("client-all " + msg);
+        notifyAll(oss.str());
     }
 
     void updateEditorSpeeds(int id, int speed) override
