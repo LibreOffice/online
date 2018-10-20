e11293cb70a2a7523038104e9850ae4d542a80ce
Tamás Zolnai <tamas.zolnai@collabora.com>
2018-10-20 12:57:53 +0200

    Enable convert-to features for wopi hosts
    Change-Id: I994be7087247215b09d117df18b661906d941266
    (cherry picked from commit bb50c9e0cdcf68b010c9d312593b6dfcd4a9c93a)

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -2050,7 +2050,7 @@ private:
     {
         LOG_DBG("Wopi capabilities request: " << request.getURI());
 
-        std::string capabilities = getCapabilitiesJson();
+        std::string capabilities = getCapabilitiesJson(request.getHost());
 
         std::ostringstream oss;
         oss << "HTTP/1.1 200 OK\r\n"
@@ -2136,7 +2136,7 @@ private:
 
             std::string format = (form.has("format") ? form.get("format") : "");
 
-            if (!allowPostFrom(socket->clientAddress()))
+            if (!allowPostFrom(socket->clientAddress()) && !StorageBase::allowedWopiHost(request.getHost()) )
             {
                 LOG_ERR("client address DENY: " << socket->clientAddress().c_str());
 
@@ -2564,7 +2564,7 @@ private:
     }
 
     /// Process the capabilities.json file and return as string.
-    std::string getCapabilitiesJson()
+    std::string getCapabilitiesJson(const std::string& host)
     {
         std::shared_ptr<StreamSocket> socket = _socket.lock();
 
@@ -2588,7 +2588,7 @@ private:
         Poco::JSON::Object::Ptr features = jsonFile.extract<Poco::JSON::Object::Ptr>();
         Poco::JSON::Object::Ptr convert_to = features->get("convert-to").extract<Poco::JSON::Object::Ptr>();
 
-        Poco::Dynamic::Var available = allowPostFrom(socket->clientAddress());
+        Poco::Dynamic::Var available = allowPostFrom(socket->clientAddress()) || StorageBase::allowedWopiHost(host);
         convert_to->set("available", available);
 
         std::ostringstream ostrJSON;
