e7c226ea18e8c112a2172a8290e6933f98904108
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2018-11-09 08:38:23 +0100

    vereign logging in and logging out
    Change-Id: I33427c1451ae795e3692cb96a95488d85afb2db4

--- loleaflet/js/toolbar.js
+++ loleaflet/js/toolbar.js
@@ -228,6 +228,9 @@ function onClick(e, id, item, subItem) {
 			map.sendUnoCommand('.uno:StatusBarFunc', command);
 		});
 	}
+	else if (id === 'logout') {
+		map.signingLogout();
+	}
 }
 
 function setBorders(left, right, bottom, top, horiz, vert) {
