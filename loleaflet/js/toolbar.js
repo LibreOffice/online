21888a00c8290e9e1186c9040184080dba6a29c1
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2018-11-15 09:47:37 +0100

    separate lib. init. from login, show UI elements on state change
    It is needed to separate initialization of the library and login
    (the button) as you may already be logged in if you have
    credentials in the local storage (already logged in through
    vereign website for example). So behaviour now is either you get
    logged in immediately or you get a login button.
    
    There is a lot of state changes where various elements of the
    infobar are show or hidden. To make it easier to track the changes
    it is necessary to have showing/hiding at one function which is
    triggered every time a state change may occur.
    
    Change-Id: I0f36f342baaf80dc109f608e294b89a955a6572e

--- loleaflet/js/toolbar.js
+++ loleaflet/js/toolbar.js
@@ -962,8 +962,8 @@ function initNormalToolbar(toolItems) {
 				{type: 'html', id: 'logo', html: '<p><b>Vereign</b></p>'},
 				{type: 'button',  id: 'sign',  caption: 'Sign', img: '', hint: _('Sign document')},
 				{type: 'break' },
-				{type: 'html', id: 'user-label', html: '<p>User:</p>'},
-				{type: 'html', id: 'user', html: '<none>'},
+				{type: 'html', id: 'identity-label', html: '<b>Identity:</b>'},
+				{type: 'html', id: 'identity', html: 'N/A'},
 				{type: 'break' },
 				{type: 'button',  id: 'logout',  caption: 'Logout', img: '', hint: _('Logout')},
 				{type: 'button',  id: 'login',  caption: 'Login', img: '', hint: _('Login')},
@@ -1481,8 +1481,6 @@ function onDocLayerInit() {
 		$('#spreadsheet-toolbar').hide();
 		$('#presentation-toolbar').hide();
 
-		w2ui['document-signing-bar'].hide('logout');
-
 		break;
 	case 'presentation':
 		var presentationToolbar = w2ui['presentation-toolbar'];
@@ -1520,6 +1518,8 @@ function onDocLayerInit() {
 
 		break;
 	}
+
+	map.signingInitializeBar();
 
 	if (L.Browser.mobile) {
 		_mobilify();
