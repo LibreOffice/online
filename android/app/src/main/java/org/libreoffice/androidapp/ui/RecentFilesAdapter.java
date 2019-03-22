ea177bcbcb1904a54310c770ae3e47b663e6bab3
Florin Ciornei <florin.ciornei@collabora.com>
2019-03-22 17:24:47 +0100

    Port document browser from the old Java app to the new one.
    Edit FAB still doesn't work, and the opencloud document browser should be
    implemented
    
    Change-Id: I788bf271b543e397cb156b970116614e73f1ad95

---
+++ android/app/src/main/java/org/libreoffice/androidapp/ui/RecentFilesAdapter.java
@@ -0,0 +1,94 @@
+/* -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.libreoffice.androidapp.ui;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.libreoffice.androidapp.R;
+import org.libreoffice.androidapp.storage.IFile;
+
+import java.util.List;
+
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+class RecentFilesAdapter extends RecyclerView.Adapter<RecentFilesAdapter.ViewHolder> {
+
+    private LibreOfficeUIActivity mActivity;
+    private List<IFile> recentFiles;
+
+    RecentFilesAdapter(LibreOfficeUIActivity activity, List<IFile> recentFiles) {
+        this.mActivity = activity;
+        this.recentFiles = recentFiles;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_recent_files, parent, false);
+        return new ViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final IFile iFile = recentFiles.get(position);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mActivity.open(iFile);
+            }
+        });
+
+        String filename = iFile.getName();
+
+        holder.textView.setText(filename);
+
+        int compoundDrawableInt = 0;
+
+        switch (FileUtilities.getType(filename)) {
+            case FileUtilities.DOC:
+                compoundDrawableInt = R.drawable.writer;
+                break;
+            case FileUtilities.CALC:
+                compoundDrawableInt = R.drawable.calc;
+                break;
+            case FileUtilities.DRAWING:
+                compoundDrawableInt = R.drawable.draw;
+                break;
+            case FileUtilities.IMPRESS:
+                compoundDrawableInt = R.drawable.impress;
+                break;
+        }
+        holder.imageView.setImageDrawable(ContextCompat.getDrawable(mActivity, compoundDrawableInt));
+    }
+
+    @Override
+    public int getItemCount() {
+        return recentFiles.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textView;
+        ImageView imageView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            this.textView = itemView.findViewById(R.id.textView);
+            this.imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
