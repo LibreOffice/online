LibreOffice/online

commit 1d2b0415b92f629ea4e73cb2265cba0bca6e2111

loleaflet: remove "document changes" status bar item
It is no longer need it

Change-Id: Ic054c52ad14c55a0e93c84f4ff94f83ea0d33100

loleaflet/src/control/Control.Toolbar.js
@@ -995,8 +995,6 @@ function initNormalToolbar() {
 				{type: 'button',  id: 'cancelsearch', img: 'cancel', hint: _('Cancel the search'), hidden: true},
 				{type: 'html',  id: 'left'},
 				{type: 'html',  id: 'right'},
-				{type: 'html',  id: 'modifiedstatuslabel', html: '<div id="modifiedstatuslabel" class="loleaflet-font"></div>', mobile: false, tablet: false},
-				{type: 'break', id: 'modifiedstatuslabelbreak', mobile: false},
 				{type: 'drop', id: 'userlist', img: 'users', hidden: true, html: '<div id="userlist_container"><table id="userlist_table"><tbody></tbody></table>' +
 					'<hr><table class="loleaflet-font" id="editor-btn">' +
 					'<tr>' +
@@ -1678,19 +1676,6 @@ function onCommandStateChanged(e) {
 	}
 	else if (commandName === '.uno:LanguageStatus') {
 		updateToolbarItem(statusbar, 'LanguageStatus', $('#LanguageStatus').html(_(state)).parent().html());
-	}
-	else if (commandName === '.uno:ModifiedStatus') {
-		var modifiedStatus = e.state === 'true';
-		var html;
-		if (modifiedStatus) {
-			html = $('#modifiedstatuslabel').html('Document modified').parent().html();
-			w2ui['editbar'].set('save', {img:'savemodified'});
-		}
-		else {
-			html = $('#modifiedstatuslabel').html(_('Document saved')).parent().html();
-			w2ui['editbar'].set('save', {img:'save'});
-		}
-		updateToolbarItem(statusbar, 'modifiedstatuslabel', html);
 	}
 	else if (commandName === '.uno:StatusDocPos') {
 		state = toLocalePattern('Sheet %1 of %2', 'Sheet (\\d+) of (\\d+)', state, '%1', '%2');

commit dfe0b4b0ec4562c998e2e305732252af7e80ab12

Update Save icon when document is changed
It was not showing modified status.

Regression since 1d2b0415b92f629ea4e73cb2265cba0bca6e2111

Change-Id: I5604dc87613fc5052b5932aaabf470fc4fb8d1bf

loleaflet/src/control/Control.Toolbar.js
@@ -1677,6 +1677,14 @@ function onCommandStateChanged(e) {
 	else if (commandName === '.uno:LanguageStatus') {
 		updateToolbarItem(statusbar, 'LanguageStatus', $('#LanguageStatus').html(_(state)).parent().html());
 	}
+	else if (commandName === '.uno:ModifiedStatus') {
+		if (e.state === 'true') {
+			w2ui['editbar'].set('save', {img:'savemodified'});
+		}
+		else {
+			w2ui['editbar'].set('save', {img:'save'});
+		}
+	}
 	else if (commandName === '.uno:StatusDocPos') {
 		state = toLocalePattern('Sheet %1 of %2', 'Sheet (\\d+) of (\\d+)', state, '%1', '%2');
 		updateToolbarItem(statusbar, 'StatusDocPos', $('#StatusDocPos').html(state ? state : '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp').parent().html());