87309c610f221ed226c381a57e67dde011e277af
Iván Sánchez Ortega <ivan.sanchez@collabora.com>
2019-05-06 17:01:51 +0200

    loleaflet: Change setTimeout to setInterval in mCustomScrollbar
    This *should* fix a bug where timers eat up 20% oc FPU time after
    leafing loleaflet open for a long time.
    
    The current nested setTimeout seems to work fine, but the hypothesis
    here is that browsers that keep async stack traces are saving a
    constantly-growing stack trace, so that the real slow-down doesn't
    come from the JS code, but rather from the browser's development tools.
    
    There seems to be no benefit from patching upstream, as mCustomScrollbar
    seems to be unmaintained since 2016.
    
    Change-Id: Ie496cd7dcedf443911655939064c54ebb76c2bbc

--- loleaflet/js/jquery.mCustomScrollbar.js
+++ loleaflet/js/jquery.mCustomScrollbar.js
@@ -1933,53 +1933,58 @@ and dependencies (minified).
 				removes autoUpdate timer 
 				usage: _autoUpdate.call(this,"remove");
 				*/
-				clearTimeout(mCSB_container[0].autoUpdate);
+				clearInterval(mCSB_container[0].autoUpdate);
 				_delete(mCSB_container[0],"autoUpdate");
 				return;
 			}
-			upd();
-			function upd(){
-				clearTimeout(mCSB_container[0].autoUpdate);
+			
+			clearInterval(mCSB_container[0].autoUpdate);
+
+			mCSB_container[0].autoUpdate=setInterval(function upd(){
 				if($this.parents("html").length===0){
 					/* check element in dom tree */
+					clearInterval(mCSB_container[0].autoUpdate);
 					$this=null;
 					return;
 				}
-				mCSB_container[0].autoUpdate=setTimeout(function(){
-					/* update on specific selector(s) length and size change */
-					if(o.advanced.updateOnSelectorChange){
-						d.poll.change.n=sizesSum();
-						if(d.poll.change.n!==d.poll.change.o){
-							d.poll.change.o=d.poll.change.n;
-							doUpd(3);
+				
+				/* update on specific selector(s) length and size change */
+				if(o.advanced.updateOnSelectorChange){
+					d.poll.change.n=sizesSum();
+					if(d.poll.change.n!==d.poll.change.o){
+						d.poll.change.o=d.poll.change.n;
+						doUpd(3);
+						return;
+					}
+				}
+				/* update on main element and scrollbar size changes */
+				if(o.advanced.updateOnContentResize){
+					d.poll.size.n=$this[0].scrollHeight+$this[0].scrollWidth+mCSB_container[0].offsetHeight+$this[0].offsetHeight+$this[0].offsetWidth;
+					if(d.poll.size.n!==d.poll.size.o){
+						d.poll.size.o=d.poll.size.n;
+						doUpd(1);
+						return;
+					}
+				}
+				/* update on image load */
+				if(o.advanced.updateOnImageLoad){
+					if(!(o.advanced.updateOnImageLoad==="auto" && o.axis==="y")){ //by default, it doesn't run on vertical content
+						d.poll.img.n=mCSB_container.find("img").length;
+						if(d.poll.img.n!==d.poll.img.o){
+							d.poll.img.o=d.poll.img.n;
+							mCSB_container.find("img").each(function(){
+								imgLoader(this);
+							});
 							return;
 						}
 					}
-					/* update on main element and scrollbar size changes */
-					if(o.advanced.updateOnContentResize){
-						d.poll.size.n=$this[0].scrollHeight+$this[0].scrollWidth+mCSB_container[0].offsetHeight+$this[0].offsetHeight+$this[0].offsetWidth;
-						if(d.poll.size.n!==d.poll.size.o){
-							d.poll.size.o=d.poll.size.n;
-							doUpd(1);
-							return;
-						}
-					}
-					/* update on image load */
-					if(o.advanced.updateOnImageLoad){
-						if(!(o.advanced.updateOnImageLoad==="auto" && o.axis==="y")){ //by default, it doesn't run on vertical content
-							d.poll.img.n=mCSB_container.find("img").length;
-							if(d.poll.img.n!==d.poll.img.o){
-								d.poll.img.o=d.poll.img.n;
-								mCSB_container.find("img").each(function(){
-									imgLoader(this);
-								});
-								return;
-							}
-						}
-					}
-					if(o.advanced.updateOnSelectorChange || o.advanced.updateOnContentResize || o.advanced.updateOnImageLoad){upd();}
-				},o.advanced.autoUpdateTimeout);
-			}
+				}
+				if (!(o.advanced.updateOnSelectorChange || o.advanced.updateOnContentResize || o.advanced.updateOnImageLoad)) {
+					clearInterval(mCSB_container[0].autoUpdate);
+				}
+			},o.advanced.autoUpdateTimeout);
+			
+
 			/* a tiny image loader */
 			function imgLoader(el){
 				if($(el).hasClass(classes[2])){doUpd(); return;}
@@ -2004,7 +2009,7 @@ and dependencies (minified).
 			}
 			/* calls the update method */
 			function doUpd(cb){
-				clearTimeout(mCSB_container[0].autoUpdate);
+				clearInterval(mCSB_container[0].autoUpdate);
 				methods.update.call(null,$this[0],cb);
 			}
 		},
