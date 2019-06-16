bc21b4e79cc90d7d6ba6aaa20e95ae8f6fb5f8ee
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2019-06-16 14:42:11 -0400

    Pass Extended Data in Action_Save from the client to the WOPI host
    Clients often need to communicate with their wopi hosts when
    invoking Action_Save to provide more context when storing the
    document in question. Action_Save now support passing arbitrary
    string as ExtendedData entry that can be used by client to
    pass any context or otherwise flags to the WOPI host, which
    will receive it via the X-LOOL-WOPI-ExtendedData custom header.
    
    See reference.html for more details.
    
    Change-Id: I1814d1f3d984a553ffa60cec13d23b014ba59eb3

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -1037,7 +1037,8 @@ bool DocumentBroker::autoSave(const bool force)
 }
 
 bool DocumentBroker::sendUnoSave(const std::string& sessionId, bool dontTerminateEdit,
-                                 bool dontSaveIfUnmodified, bool isAutosave, bool isExitSave)
+                                 bool dontSaveIfUnmodified, bool isAutosave, bool isExitSave,
+                                 const std::string& extendedData)
 {
     assertCorrectThread();
 
@@ -1081,6 +1082,7 @@ bool DocumentBroker::sendUnoSave(const std::string& sessionId, bool dontTerminat
         assert(_storage);
         _storage->setIsAutosave(isAutosave || UnitWSD::get().isAutosave());
         _storage->setIsExitSave(isExitSave);
+        _storage->setExtendedData(extendedData);
 
         const std::string saveArgs = oss.str();
         LOG_TRC(".uno:Save arguments: " << saveArgs);
