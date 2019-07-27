fe98acef4fb9880eb482b2692ed49dc0b229885c
kaishu-sahu <kaishusahu101@gmail.com>
2019-07-28 05:13:26 +0530

    android: add support for screen dimming from JS.
    Change-Id: Icd766c7dcc9c2d62ec90945ec88fa46f88a4711d

--- loleaflet/src/map/Map.js
+++ loleaflet/src/map/Map.js
@@ -1089,6 +1089,11 @@ L.Map = L.Evented.extend({
 		this._active = false;
 		clearTimeout(vex.timer);
 
+		if (window.ThisIsTheAndroidApp) {
+			window.postMobileMessage('DIM_SCREEN');
+			return;
+		}
+
 		var message = '';
 		var map = this;
 		if (!map['wopi'].DisableInactiveMessages) {
@@ -1116,6 +1121,9 @@ L.Map = L.Evented.extend({
 
 	notifyActive : function() {
 		this.lastActiveTime = Date.now();
+		if (window.ThisIsTheAndroidApp) {
+			window.postMobileMessage('LIGHT_SCREEN');
+		}
 	},
 
 	_dimIfInactive: function () {
