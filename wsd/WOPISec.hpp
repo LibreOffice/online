0c9240d0955682b2c134f12047588af681ec4405
Mike Kaganski <mike.kaganski@collabora.com>
2019-11-07 18:24:37 +0300

    Implement WOPI proof
    Currently requires manual generation of an RSA key, e.g. using
    
        ssh-keygen -t rsa -N "" -f WOPI_proof_key
    
    Change-Id: I7ab79218ca2af268dd4573cb64c6353dc71b5f03

---
+++ wsd/WOPISec.hpp
@@ -0,0 +1,31 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+// WOPI proof management
+#ifndef INCLUDED_WOPISEC_HPP
+#define INCLUDED_WOPISEC_HPP
+
+#include <string>
+#include <chrono>
+#include <Poco/Net/HTTPRequest.h>
+
+int64_t DotNetTicks(const std::chrono::system_clock::time_point& utc);
+std::string GetWopiProof(
+    const std::string& access_token, // utf-8
+    const std::string& uri, // utf-8
+    int64_t ticks); // .Net ticks
+std::string SignWopiProof(const std::string proof);
+std::string GetWopiProofKey();
+std::string GetWopiProofModulus();
+std::string GetWopiProofExponent();
+std::string GetWopiSecHeaders(Poco::Net::HTTPRequest& request);
+
+#endif // INCLUDED_WOPISEC_HPP
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
