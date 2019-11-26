fdd73e3c7c34e0d93288951b961c42d6910b5806
Szymon Kłos <szymon.klos@collabora.com>
2019-11-26 15:26:31 +0100

    jsdialog: handle graphic items updates
    Change-Id: I3a04cefe34ce8ca8806d93a72e047563d9c4c26b

--- loleaflet/src/control/Control.JSDialogBuilder.js
+++ loleaflet/src/control/Control.JSDialogBuilder.js
@@ -506,6 +506,24 @@ L.Control.JSDialogBuilder = L.Control.extend({
 
 		case 'gradientstyle':
 			return '.uno:FillGradient';
+
+		case 'setbrightness':
+			return '.uno:GrafLuminance';
+
+		case 'setcontrast':
+			return '.uno:GrafContrast';
+
+		case 'setred':
+			return '.uno:GrafRed';
+
+		case 'setgreen':
+			return '.uno:GrafGreen';
+
+		case 'setblue':
+			return '.uno:GrafBlue';
+
+		case 'setgamma':
+			return '.uno:GrafGamma';
 		}
 
 		return null;
@@ -658,6 +676,48 @@ L.Control.JSDialogBuilder = L.Control.extend({
 			state = items.getItemValue('.uno:FillGradient');
 			if (state) {
 				return state.angle;
+			}
+			break;
+
+		case 'setbrightness':
+			state = items.getItemValue('.uno:GrafLuminance');
+			if (state) {
+				return state.replace(',', '.');
+			}
+			break;
+
+		case 'setcontrast':
+			state = items.getItemValue('.uno:GrafContrast');
+			if (state) {
+				return state.replace(',', '.');
+			}
+			break;
+
+		case 'setred':
+			state = items.getItemValue('.uno:GrafRed');
+			if (state) {
+				return state.replace(',', '.');
+			}
+			break;
+
+		case 'setgreen':
+			state = items.getItemValue('.uno:GrafGreen');
+			if (state) {
+				return state.replace(',', '.');
+			}
+			break;
+
+		case 'setblue':
+			state = items.getItemValue('.uno:GrafBlue');
+			if (state) {
+				return state.replace(',', '.');
+			}
+			break;
+
+		case 'setgamma':
+			state = items.getItemValue('.uno:GrafGamma');
+			if (state) {
+				return state.replace(',', '.');
 			}
 			break;
 		}
