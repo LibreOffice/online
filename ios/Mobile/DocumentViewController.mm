f1fdc2209851ed970937683020a38a34a7290c1e
Jan Holesovsky <kendy@collabora.com>
2019-12-05 22:27:16 +0100

    mobile: Unify the mutex usage that protects the main lokit thread.
    Effectively both approaches were doing the same thing, let's unify to
    the iOS way to minimize the platform-specific code.
    
    Change-Id: I11290410a536c26db054ffcb87e3b64cc2a11c07

--- ios/Mobile/DocumentViewController.mm
+++ ios/Mobile/DocumentViewController.mm
@@ -16,6 +16,7 @@
 
 #import "ios.h"
 #import "FakeSocket.hpp"
+#import "LOOLWSD.hpp"
 #import "Log.hpp"
 #import "SigUtil.hpp"
 #import "Util.hpp"
@@ -370,7 +371,7 @@ static DocumentViewController* theSingleton = nil;
            }];
 
     // Wait for lokit_main thread to exit
-    std::lock_guard<std::mutex> lock(lokit_main_mutex);
+    std::lock_guard<std::mutex> lock(LOOLWSD::lokit_main_mutex);
 
     theSingleton = nil;
 
