94a95937cec7ad4a253810b4a831d5a9d795837a
Jan Holesovsky <kendy@collabora.com>
2019-12-05 22:27:16 +0100

    mobile: Unify the mutex usage that protects the main lokit thread.
    Effectively both approaches were doing the same thing, let's unify to
    the iOS way to minimize the platform-specific code.
    
    Change-Id: I11290410a536c26db054ffcb87e3b64cc2a11c07

--- android/lib/src/main/cpp/androidapp.cpp
+++ android/lib/src/main/cpp/androidapp.cpp
@@ -34,7 +34,6 @@ static int fakeClientFd;
 static int closeNotificationPipeForForwardingThread[2] = {-1, -1};
 static JavaVM* javaVM = nullptr;
 static bool lokInitialized = false;
-static std::mutex loolwsdRunningMutex;
 
 extern "C" JNIEXPORT jint JNICALL
 JNI_OnLoad(JavaVM* vm, void*) {
@@ -147,12 +146,11 @@ static void send2JS(jclass loActivityClz, jobject loActivityObj, const std::vect
 void closeDocument()
 {
     // Close one end of the socket pair, that will wake up the forwarding thread that was constructed in HULLO
-    if (fakeSocketClose(closeNotificationPipeForForwardingThread[0]) == 0)
-    {
-        LOG_DBG("Waiting for LOOLWSD to finish...");
-        std::unique_lock<std::mutex> lock(loolwsdRunningMutex);
-        LOG_DBG("LOOLWSD has finished.");
-    }
+    fakeSocketClose(closeNotificationPipeForForwardingThread[0]);
+
+    LOG_DBG("Waiting for LOOLWSD to finish...");
+    std::unique_lock<std::mutex> lock(LOOLWSD::lokit_main_mutex);
+    LOG_DBG("LOOLWSD has finished.");
 }
 
 /// Handle a message from JavaScript.
@@ -308,7 +306,6 @@ Java_org_libreoffice_androidlib_LOActivity_createLOOLWSD(JNIEnv *env, jobject, j
                     {
                         LOG_DBG("Creating LOOLWSD");
                         {
-                            std::unique_lock<std::mutex> lock(loolwsdRunningMutex);
                             fakeClientFd = fakeSocketSocket();
                             LOG_DBG("createLOOLWSD created fakeClientFd: " << fakeClientFd);
                             std::unique_ptr<LOOLWSD> loolwsd(new LOOLWSD());
