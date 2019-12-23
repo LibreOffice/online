669c449830704947207c97a753e25e1e48c44149
Pranam Lashkari <lpranam@collabora.com>
2019-12-23 19:15:38 +0530

    Resolved: Server crash when link opened in new tab
    When trying to open a link normally from help->Online help
    nothing happens but the popup is closed.
    
    When trying to open a like forcefully in new tab
    from help->online help it crashes the server.
    
    Change-Id: I7e0944ebe521002625a84e155e379ed7e25d2309

--- common/Authorization.cpp
+++ common/Authorization.cpp
@@ -11,6 +11,7 @@
 
 #include "Authorization.hpp"
 #include "Protocol.hpp"
+#include "Log.hpp"
 
 #include <cstdlib>
 #include <cassert>
@@ -77,7 +78,10 @@ void Authorization::authorizeRequest(Poco::Net::HTTPRequest& request) const
             break;
         }
         default:
-            assert(false);
+            // assert(false);
+            LOG_ERR("Request authentication failed!");
+            throw;
+            break;
     }
 }
 
