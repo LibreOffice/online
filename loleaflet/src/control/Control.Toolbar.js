LibreOffice/online

commit 2f861612e9aac7e92722597f345a6ab4f4e00d02

Some menubar folding/unfolding cleanups
Change-Id: I4e5abe59076a5dbd41b1c8d002c0f3c4e53569de
Reviewed-on: https://gerrit.libreoffice.org/72858
Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
Tested-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

loleaflet/src/control/Control.Toolbar.js
@@ -1561,16 +1561,10 @@ function onDocLayerInit() {
 	statusbar.refresh();
 
 	if (window.mode.isTablet()) {
-		// Fold menubar by default
-		// FIXME: reuse toogleMenubar / use css
-		$('.main-nav').css({'display': 'none'});
-		$('#closebuttonwrapper').css({'display': 'none'});
-		var obj = $('.fold');
-		obj.removeClass('w2ui-icon fold');
-		obj.addClass('w2ui-icon unfold');
+		map.hideMenubar();
+
 		$('#document-container').addClass('tablet');
 		$('#spreadsheet-row-column-frame').addClass('tablet');
-		$('#presentation-controls-wrapper').css({'top': '41px'});
 
 		$('#tb_editbar_item_fonts').css({'display': 'none'});
 		$('#tb_editbar_item_fontsizes').css({'display': 'none'});

loleaflet/src/map/Map.js
@@ -1466,8 +1466,8 @@ L.Map = L.Evented.extend({
 		}
 
 		var obj = $('.unfold');
-		obj.removeClass('w2ui-icon unfold');
-		obj.addClass('w2ui-icon fold');
+		obj.removeClass('unfold');
+		obj.addClass('fold');
 
 		moveObjectVertically($('#spreadsheet-row-column-frame'), 36);
 		moveObjectVertically($('#document-container'), 36);
@@ -1483,8 +1483,8 @@ L.Map = L.Evented.extend({
 		}
 
 		var obj = $('.fold');
-		obj.removeClass('w2ui-icon fold');
-		obj.addClass('w2ui-icon unfold');
+		obj.removeClass('fold');
+		obj.addClass('unfold');
 
 		moveObjectVertically($('#spreadsheet-row-column-frame'), -36);
 		moveObjectVertically($('#document-container'), -36);

commit 2a0a9872fe329e37a73733fdcb98262b81f11900

tdf#130223: Revert "Some menubar folding/unfolding cleanups"
This reverts commit 2f861612e9aac7e92722597f345a6ab4f4e00d02,
which caused the formula bar to be hidden, and rows to get
misaligned on tablets.

Change-Id: I14826fccb23283e3f7462eee7aeb43fb6d2eaeaa

loleaflet/src/control/Control.Toolbar.js
@@ -1714,10 +1714,16 @@ function onDocLayerInit() {
 	}
 
 	if (!window.ThisIsTheiOSApp && window.mode.isTablet()) {
-		map.hideMenubar();
-
+		// Fold menubar by default
+		// FIXME: reuse toogleMenubar / use css
+		$('.main-nav').css({'display': 'none'});
+		$('#closebuttonwrapper').css({'display': 'none'});
+		var obj = $('.fold');
+		obj.removeClass('w2ui-icon fold');
+		obj.addClass('w2ui-icon unfold');
 		$(map.options.documentContainer).addClass('tablet');
 		$('#spreadsheet-row-column-frame').addClass('tablet');
+		$('#presentation-controls-wrapper').css({'top': '41px'});
 
 		$('#tb_editbar_item_fonts').css({'display': 'none'});
 		$('#tb_editbar_item_fontsizes').css({'display': 'none'});

loleaflet/src/map/Map.js
@@ -1681,8 +1681,8 @@ L.Map = L.Evented.extend({
 		}
 
 		var obj = $('.unfold');
-		obj.removeClass('unfold');
-		obj.addClass('fold');
+		obj.removeClass('w2ui-icon unfold');
+		obj.addClass('w2ui-icon fold');
 
 		moveObjectVertically($('#spreadsheet-row-column-frame'), 36);
 		moveObjectVertically($(this.options.documentContainer), 36);
@@ -1699,8 +1699,8 @@ L.Map = L.Evented.extend({
 		}
 
 		var obj = $('.fold');
-		obj.removeClass('fold');
-		obj.addClass('unfold');
+		obj.removeClass('w2ui-icon fold');
+		obj.addClass('w2ui-icon unfold');
 
 		moveObjectVertically($('#spreadsheet-row-column-frame'), -36);
 		moveObjectVertically($(this.options.documentContainer), -36);