fa2c01e6e12ae11744956c6f538de9b4de22d7ec
Pranam Lashkari <lpranam@collabora.com>
2020-01-30 04:08:58 +0530

    Fix: mobile wizard malfunctioning
    The mobile wizard would not perform any actions
    if opened when the mobile keyboard is visible
    
    Change-Id: Id8722b551ea6c13f3fe22b78c996e2bb4b2db2f8

--- loleaflet/src/control/Control.ColumnHeader.js
+++ loleaflet/src/control/Control.ColumnHeader.js
@@ -391,10 +391,12 @@ L.Control.ColumnHeader = L.Control.Header.extend({
 		this._canvasContext.clearRect(0, 0, canvas.width, canvas.height);
 
 		// Reset state
-		this._current = -1;
-		this._selection.start = this._selection.end = -1;
-		this._mouseOverEntry = null;
-		this._lastMouseOverIndex = undefined;
+		if (!window.contextMenuWizard) {
+			this._current = -1;
+			this._selection.start = this._selection.end = -1;
+			this._mouseOverEntry = null;
+			this._lastMouseOverIndex = undefined;
+		}
 
 		// create data structure for column widths
 		this._tickMap = new L.Control.Header.GapTickMap(this._map, columns);
