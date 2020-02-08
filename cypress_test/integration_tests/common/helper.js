d8ecde9ff25fedf8e4b4e339e621f205b3296470
Tamás Zolnai <tamas.zolnai@collabora.com>
2020-02-08 11:18:57 +0100

    For Jenkins: check how cypress tests run.
    Change-Id: Iea1292bf44d77403356b3e9800c2af4c2237d08d

--- cypress_test/integration_tests/common/helper.js
+++ cypress_test/integration_tests/common/helper.js
@@ -68,12 +68,12 @@ function copyTextToClipboard() {
 		.then(function(marker) {
 			expect(marker).to.have.lengthOf(2);
 			var XPos =  (marker[0].getBoundingClientRect().right + marker[1].getBoundingClientRect().left) / 2;
-			var YPos = marker[0].getBoundingClientRect().top - 5;
+			var YPos = marker[0].getBoundingClientRect().top - 3;
 			longPressOnDocument(XPos, YPos);
 		});
 
 	// Execute copy
-	cy.get('.ui-header.level-0.mobile-wizard.ui-widget .menu-entry-with-icon .context-menu-link')
+	cy.get('.ui-header.level-0.mobile-wizard.ui-widget .menu-entry-with-icon .context-menu-link', {timeout : 10000})
 		.contains('Copy')
 		.click();
 
