b818252f484576175d7526842b5622d7217ec32a
Marco Cecchetti <marco.cecchetti@collabora.com>
2020-02-25 14:03:46 +0100

    loleaflet: remove sum and equal buttons from the formula bar
    The sum and equal buttons has been removed from the formula bar.
    Now, in the mobile case, the accept/cancel buttons are shown in place
    of undo/redo buttons when the formula input bar gets focus.
    
    Change-Id: I378059396df3566d5bf3c83b2defa99fc669054d

--- loleaflet/src/map/Map.js
+++ loleaflet/src/map/Map.js
@@ -1387,6 +1387,7 @@ L.Map = L.Evented.extend({
 		if (this.dialog._calcInputBar) {
 			var inputBarId = this.dialog._calcInputBar.id;
 			this.dialog._updateTextSelection(inputBarId);
+			window.onFormulaBarBlur();
 		}
 	},
 
