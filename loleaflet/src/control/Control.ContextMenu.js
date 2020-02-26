LibreOffice/online

commit b36d56bf5b022bf4b41d062a82326b5ea57b8dbf

Context menu wizard: wait a small time before showing context menu.
Otherwise a cursor invalidation can close the wizard just after
it is opened.

Change-Id: I3e153ca05a0902b9b2225bbb6169062b79eebdd0
Reviewed-on: https://gerrit.libreoffice.org/84648
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Tamás Zolnai <tamas.zolnai@collabora.com>

loleaflet/src/control/Control.ContextMenu.js
@@ -3,7 +3,7 @@
 * Control.ContextMenu
 */
 
-/* global $ _ _UNO */
+/* global $ _ _UNO vex */
 L.Control.ContextMenu = L.Control.extend({
 	options: {
 		SEPARATOR: '---------',
@@ -101,20 +101,27 @@ L.Control.ContextMenu = L.Control.extend({
 		if (this.hasContextMenu) {
 			this._onClosePopup();
 		}
+
 		var contextMenu = this._createContextMenuStructure(obj);
+		var spellingContextMenu = false;
+		for (var menuItem in contextMenu) {
+			if (menuItem.includes('.uno:SpellCheckIgnore')) {
+				spellingContextMenu = true;
+				break;
+			}
+		}
 		if (window.mode.isMobile()) {
 			window.contextMenuWizard = true;
 			var menuData = this.getMenuStructureForMobileWizard(contextMenu, true, '');
-			map.fire('mobilewizard', menuData);
-		} else {
-			var spellingContextMenu = false;
-			for (var menuItem in contextMenu) {
-				if (menuItem.includes('.uno:SpellCheckIgnore')) {
-					spellingContextMenu = true;
-					break;
-				}
+			if (spellingContextMenu === true) {
+				vex.timer = setInterval(function() {
+					map.fire('mobilewizard', menuData);
+					clearTimeout(vex.timer);
+				}, 200);
+			} else {
+				map.fire('mobilewizard', menuData);
 			}
-
+		} else {
 			L.installContextMenu({
 				selector: '.leaflet-layer',
 				className: 'loleaflet-font',

commit d38eb7da6dd67b7161275fe5266612262ef4ddf8

Replace 'includes' with '$.inArray' for IE11
Regression from b36d56bf5b022bf4b41d062a82326b5ea57b8dbf.

Change-Id: I270afb92366fa8992c08a79bd1c561117a18bb9c

loleaflet/src/control/Control.ContextMenu.js
@@ -109,7 +109,7 @@ L.Control.ContextMenu = L.Control.extend({
 		var contextMenu = this._createContextMenuStructure(obj);
 		var spellingContextMenu = false;
 		for (var menuItem in contextMenu) {
-			if (menuItem.includes('.uno:SpellCheckIgnore')) {
+			if ($.inArray('.uno:SpellCheckIgnore', menuItem)) {
 				spellingContextMenu = true;
 				break;
 			}