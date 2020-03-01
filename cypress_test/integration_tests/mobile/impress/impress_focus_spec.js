c6e2028a69f730a43a4dfe6461ae337be56465ad
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2020-03-01 10:48:10 -0500

    cypress: new impress editing tests
    Single- and double-click starts editing
    text-boxes in Impress. Single-clicking works
    only on text, not in the white-space.
    
    And improved helpers to minimize duplication
    and update a single place when there are changes
    to the common operations/checks.
    
    Change-Id: I14ee65b18e310f4ff6b93ce81580b3d6aba0fa03

--- cypress_test/integration_tests/mobile/impress/impress_focus_spec.js
+++ cypress_test/integration_tests/mobile/impress/impress_focus_spec.js
@@ -1,6 +1,7 @@
-/* global describe it cy beforeEach require afterEach*/
+/* global describe it cy beforeEach require afterEach expect */
 
 var helper = require('../../common/helper');
+var impress = require('../../common/impress');
 
 describe('Impress focus tests', function() {
 	beforeEach(function() {
@@ -11,9 +12,87 @@ describe('Impress focus tests', function() {
 		helper.afterAll();
 	});
 
+	it('Select text box, no editing', function() {
+
+		helper.enableEditingMobile();
+
+		impress.assertNotInTextEditMode();
+
+		// One tap on a text shape, on the whitespace area,
+		// does not start editing.
+		cy.get('#document-container')
+			.then(function(items) {
+				expect(items).have.length(1);
+
+				// Click in the top left corner where there is no text.
+				var posX = items[0].getBoundingClientRect().left + items[0].getBoundingClientRect().width / 4;
+				var posY = items[0].getBoundingClientRect().top + items[0].getBoundingClientRect().height / 4;
+				cy.log('Got first quartile at (' + posX + ', ' + posY + ')');
+
+				cy.get('#document-container')
+					.click(posX, posY);
+			});
+
+		// Shape selection.
+		cy.get('.leaflet-pane.leaflet-overlay-pane svg g')
+			.should('exist');
+
+		// But no editing.
+		impress.assertNotInTextEditMode();
+	});
+
+	it('Single- and Double-click to edit', function() {
+
+		helper.enableEditingMobile();
+
+		impress.assertNotInTextEditMode();
+
+		// Enter edit mode by double-clicking.
+		cy.get('#document-container')
+			.dblclick();
+
+		impress.typeTextAndVerify('Hello Impress');
+
+		// Now test single-click editing, by ending the previous
+		// editing and single-click on the text we entered.
+		cy.get('.leaflet-marker-icon')
+			.then(function(marker) {
+				// Get the center coordinates for single-click.
+				expect(marker).to.have.lengthOf(2);
+				var posX = marker[0].getBoundingClientRect().right +
+					(marker[1].getBoundingClientRect().left - marker[0].getBoundingClientRect().right) / 2;
+				var posY = marker[0].getBoundingClientRect().top - marker[0].getBoundingClientRect().height;
+				cy.log('Got text center at (' + posX + ', ' + posY + ')');
+
+				// Clear the text.
+				helper.clearAllText();
+
+				// End editing.
+				cy.get('#document-container')
+					.type('{esc}').wait(500);
+
+				impress.assertNotInTextEditMode();
+
+				// Single-click to re-edit.
+				cy.get('#document-container')
+					.then(function(items) {
+						expect(items).have.length(1);
+
+						cy.get('#document-container')
+							.click(posX, posY).wait(500);
+
+						impress.assertInTextEditMode();
+
+						helper.selectAllText();
+
+						impress.typeTextAndVerify('Bazinga Impress');
+					});
+			});
+
+	});
+
 	it('Basic document focus.', function() {
-		// Click on edit button
-		cy.get('#mobile-edit-button').click();
+		helper.enableEditingMobile();
 
 		cy.get('#tb_actionbar_item_mobile_wizard')
 			.should('not.have.class', 'disabled');
