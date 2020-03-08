2861628524ddcc3ca9bcbc3e5f310847e42e7fc2
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2020-03-08 14:33:47 -0400

    wsd: some logging improvements
    Change-Id: Icf4c4845e10f44fe1518e58ea598c2d1053b40c1

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -3645,12 +3645,11 @@ int LOOLWSD::main(const std::vector<std::string>& /*args*/)
 
     UnitWSD::get().returnValue(returnValue);
 
-    LOG_INF("Process [loolwsd] finished.");
-
 #if MOBILEAPP
     fakeSocketDumpState();
 #endif
 
+    LOG_INF("Process [loolwsd] finished.");
     return returnValue;
 }
 
