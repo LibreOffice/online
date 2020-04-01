5ad241b00b99585507e641cf218b8a6e9b2a27b2
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2020-04-01 10:12:41 +0100

    Fix log message in docker build script
    Change-Id: Ifdd931b5ae75137669126ee544d0c9eae9fe7777

--- docker/l10n-docker-nightly.sh
+++ docker/l10n-docker-nightly.sh
@@ -41,15 +41,10 @@ echo "Building core branch '$LIBREOFFICE_BRANCH'"
 if [ -z "$LIBREOFFICE_ONLINE_BRANCH" ]; then
   LIBREOFFICE_ONLINE_BRANCH="master"
 fi;
-echo "Building online branch '$LIBREOFFICE_ONLINE_BRANCH'"
+echo "Building online branch '$LIBREOFFICE_ONLINE_BRANCH' from '$LIBREOFFICE_ONLINE_REPO'"
 
 if [ -z "$LIBREOFFICE_BUILD_TARGET" ]; then
   LIBREOFFICE_BUILD_TARGET=""
-fi;
-echo "LibreOffice build target: '$LIBREOFFICE_BUILD_TARGET'"
-
-if [ -z "$LIBREOFFICE_ONLINE_REPO" ]; then
-  LIBREOFFICE_ONLINE_REPO="https://git.libreoffice.org/online"
 fi;
 echo "LibreOffice build target: '$LIBREOFFICE_BUILD_TARGET'"
 
