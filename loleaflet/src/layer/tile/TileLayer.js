LibreOffice/online

commit 3b0478baab1e58a45fc723e9265942bf5d4c10fa

Always show cursor header

loleaflet/src/layer/tile/TileLayer.js
@@ -1701,6 +1701,8 @@ L.TileLayer = L.GridLayer.extend({
 		else if (viewCursorMarker) {
 			this._viewLayerGroup.removeLayer(viewCursorMarker);
 		}
+
+		this._viewCursors[viewId].marker.showCursorHeader();
 	},
 
 	updateAllViewCursors : function() {

commit 1f483b06404d931beb92afe55739d5e64786b4ce

Avoid de-referencing non-existing marker.
This screws up cursors across views in some circumstances,
when there is no marker in one of the views.

Regression from 3b0478baab1e58a45fc723e9265942bf5d4c10fa

Change-Id: Ia1294d83801d656a919f97206b7b573a1b74b77f

loleaflet/src/layer/tile/TileLayer.js
@@ -2230,7 +2230,8 @@ L.TileLayer = L.GridLayer.extend({
 			this._viewLayerGroup.removeLayer(viewCursorMarker);
 		}
 
-		this._viewCursors[viewId].marker.showCursorHeader();
+		if (this._viewCursors[viewId].marker)
+			this._viewCursors[viewId].marker.showCursorHeader();
 	},
 
 	updateAllViewCursors : function() {