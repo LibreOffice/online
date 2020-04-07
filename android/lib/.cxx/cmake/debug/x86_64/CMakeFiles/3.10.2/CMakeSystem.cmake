ee3ed20e7800f1e289cece2045971b69df94e4fd
Gökay ŞATIR <gokaysatir@gmail.com>
2020-04-07 17:21:18 +0300

    add pretty name - Android patch
    Change-Id: If0de49884954cde26bfbe8ba1dce8844af5b30bf

---
+++ android/lib/.cxx/cmake/debug/x86_64/CMakeFiles/3.10.2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.3.0-46-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.3.0-46-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/gokay/Android/Sdk/ndk/21.0.6113669/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-1")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
