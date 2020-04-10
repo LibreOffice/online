fdb5c9b157ca4427daa8296cfac3cb630a743748
mert <mert.tumer@collabora.com>
2020-04-10 12:39:35 +0300

    android: Blind fix for an exception of getContentResolver
    Change-Id: Iec24f014a2c82b8f4d4c134b9817651f558ecedb

--- android/app/src/main/java/org/libreoffice/androidapp/ui/RecentFilesAdapter.java
+++ android/app/src/main/java/org/libreoffice/androidapp/ui/RecentFilesAdapter.java
@@ -76,6 +76,8 @@ class RecentFilesAdapter extends RecyclerView.Adapter<RecentFilesAdapter.ViewHol
             cursor = activity.getContentResolver().query(uri, null, null, null, null);
             if (cursor != null && cursor.moveToFirst())
                 filename = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+        } catch (Exception e) {
+            return null;
         } finally {
             if (cursor != null)
                 cursor.close();
