112353fdb27d6c4aeebcd940da905bd999b0877d
mert <mert.tumer@collabora.com>
2020-04-15 13:06:16 +0300

    fix Tablet: App shows temporary file name "LibreOffice..."
    Change-Id: Ib5d0102f6be1cc3d975a7daf6c18b46cdff13966

--- loleaflet/src/control/Control.Toolbar.js
+++ loleaflet/src/control/Control.Toolbar.js
@@ -1192,7 +1192,7 @@ function onDocLayerInit() {
 	_updateToolbarsVisibility();
 
 	if (window.mode.isMobile() || window.mode.isTablet()) {
-		if (!window.ThisIsAMobileApp)
+		if (window.ThisIsAMobileApp)
 			$('#document-name-input').hide();
 		else
 			$('#document-name-input').show();
