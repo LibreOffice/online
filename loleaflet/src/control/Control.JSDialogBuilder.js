af6fadabb40a7d62ff7e2d55f38b9f6b0fd41e96
Henry Castro <hcastro@collabora.com>
2020-04-23 12:09:56 -0400

    jsdialog: add metric field handler
    Change-Id: I87212e56cdfa3c85ddfa56e8aa0f814290fa49d6

--- loleaflet/src/control/Control.JSDialogBuilder.js
+++ loleaflet/src/control/Control.JSDialogBuilder.js
@@ -109,6 +109,7 @@ L.Control.JSDialogBuilder = L.Control.extend({
 		this._controlHandlers['radiobutton'] = this._radiobuttonControl;
 		this._controlHandlers['checkbox'] = this._checkboxControl;
 		this._controlHandlers['spinfield'] = this._spinfieldControl;
+		this._controlHandlers['metricfield'] = this._metricfieldControl;
 		this._controlHandlers['edit'] = this._editControl;
 		this._controlHandlers['pushbutton'] = this._pushbuttonControl;
 		this._controlHandlers['combobox'] = this._comboboxControl;
@@ -1111,6 +1112,33 @@ L.Control.JSDialogBuilder = L.Control.extend({
 		});
 
 		updateFunction();
+
+		return false;
+	},
+
+	_metricfieldControl: function(parentContainer, data, builder, customCallback) {
+		var value;
+		var controls = L.Control.JSDialogBuilder.baseSpinField(parentContainer, data, builder, customCallback);
+
+		// It listens server state changes using GetControlState
+		// to avoid unit conversion
+		builder.map.on('commandstatechanged', function(e) {
+			if (e.state[data.id]) {
+				value = e[data.id];
+				value = parseFloat(value);
+				$(controls.spinfield).attr('value', value);
+			}
+		}, this);
+
+		controls.spinfield.addEventListener('change', function() {
+			if (customCallback)
+				customCallback();
+			else
+				builder.callback('spinfield', 'value', controls.container, this.value, builder);
+		});
+
+		value = parseFloat(data.value);
+		$(controls.spinfield).attr('value', value);
 
 		return false;
 	},
