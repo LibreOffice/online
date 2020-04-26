c617c83b777119404f1e0681305c9534c4bff7bc
Tamás Zolnai <tamas.zolnai@collabora.com>
2020-04-26 13:56:57 +0200

    cypress: then(callbackFn) -> should(callbackFn)
    It's better to use should, when we need an indicator.
    
    Change-Id: I0f08c8862411e3c7627302bbda7b632b0bc0e15c

--- cypress_test/integration_tests/mobile/calc/insertion_wizard_spec.js
+++ cypress_test/integration_tests/mobile/calc/insertion_wizard_spec.js
@@ -83,7 +83,8 @@ describe('Calc insertion wizard.', function() {
 			.should('exist');
 
 		cy.get('.leaflet-pane.leaflet-overlay-pane svg')
-			.then(function(svg) {
+			.should(function(svg) {
+				expect(svg).to.have.lengthOf(1);
 				expect(svg[0].getBBox().width).to.be.greaterThan(0);
 				expect(svg[0].getBBox().height).to.be.greaterThan(0);
 			});
