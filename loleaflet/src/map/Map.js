6aa0ae821f007842c5919add4840450fae0febeb
Aron Budea <aron.budea@collabora.com>
2020-04-29 04:08:55 +0200

    tdf#132125: Pasting into comment pastes into document instead
    Ignore paste event when editing comments.
    Fix is similar to 5bc28a9d3047e47c5b7210522e9d5729524c1f0e
    
    Change-Id: I12644ca32a99557703eef63dc1b5670e68a22813

--- loleaflet/src/map/Map.js
+++ loleaflet/src/map/Map.js
@@ -968,6 +968,10 @@ L.Map = L.Evented.extend({
 		return this._isSearching;
 	},
 
+	isEditingAnnotation: function() {
+		return this._docLayer._annotations.isEdit();
+	},
+
 	_fireInitComplete: function (condition) {
 		if (this.initComplete) {
 			return;
