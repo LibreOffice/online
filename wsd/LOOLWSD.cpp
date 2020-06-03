4803ce1b7706af3c511026c10370c2fd3db2f3b0
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2020-06-03 10:43:52 -0400

    wsd: improve readability
    Change-Id: I2f85ebff783ebb799324b0aa95f5d0d023c19231

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -2405,7 +2405,7 @@ private:
                     throw Poco::FileAccessDeniedException("Admin console disabled");
 
                 try{
-                    if (!FileServerRequestHandler::isAdminLoggedIn(request, *response.get()))
+                    if (!FileServerRequestHandler::isAdminLoggedIn(request, *response))
                         throw Poco::Net::NotAuthenticatedException("Invalid admin login");
                 }
                 catch (const Poco::Net::NotAuthenticatedException& exc)
