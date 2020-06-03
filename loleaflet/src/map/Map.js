bd22a9c146bb490c9d53ded9ce91c424e8ab090c
mert <mert.tumer@collabora.com>
2020-06-03 21:16:31 +0300

    Change Show/Hide Ruler by default on first load
    Change-Id: If0bcf08f005b8461ed559d197b49dc11448344d3
    Signed-off-by: mert <mert.tumer@collabora.com>

--- loleaflet/src/map/Map.js
+++ loleaflet/src/map/Map.js
@@ -295,16 +295,23 @@ L.Map = L.Evented.extend({
 			this.initializeModificationIndicator();
 
 			// Show sidebar.
+			var map = this;
+			var uiDefaults = map['wopi'].UIDefaults[this._docLayer._docType];
 			if (this._docLayer && !this._docLoadedOnce &&
 				(this._docLayer._docType === 'spreadsheet' || this._docLayer._docType === 'text' || this._docLayer._docType === 'presentation')) {
 				// Let the first page finish loading then load the sidebar.
-				var map = this;
 				setTimeout(function () {
 					// Show the sidebar by default, but not on mobile.
 					if (window.mode.isDesktop() && !window.ThisIsAMobileApp) {
 						map._socket.sendMessage('uno .uno:SidebarShow');
 					}
 				}, 200);
+			}
+
+			// ui defaults
+			if (window.mode.isDesktop()) {
+				if (uiDefaults && !uiDefaults.ShowRulerDefault)
+					this.uiManager.hideRuler();
 			}
 
 			// We have loaded.
