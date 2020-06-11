1b5d641b199990fe7317f27dd06c35e968bfa781
Henry Castro <hcastro@collabora.com>
2020-06-11 09:35:25 -0400

    android: fix thumbnails images when portrait orientation is set
    Change-Id: I70aee6b569f78795487cbff736130e0e7413e852

--- loleaflet/src/control/Control.PartsPreview.js
+++ loleaflet/src/control/Control.PartsPreview.js
@@ -8,8 +8,8 @@ L.Control.PartsPreview = L.Control.extend({
 	options: {
 		fetchThumbnail: true,
 		autoUpdate: true,
-		maxWidth: (window.mode.isMobile() || window.mode.isTablet()) ? 60 : 180,
-		maxHeight: (window.mode.isMobile() || window.mode.isTablet()) ? 60 : 180
+		maxWidth: !window.mode.isDesktop() ? 60 : 180,
+		maxHeight: !window.mode.isDesktop() ? 60 : 180
 	},
 	partsFocused: false,
 
@@ -31,7 +31,7 @@ L.Control.PartsPreview = L.Control.extend({
 	onAdd: function (map) {
 		this._previewInitialized = false;
 		this._previewTiles = [];
-		this._direction = window.mode.isMobile() && L.DomUtil.isPortrait() ? 'x' : 'y';
+		this._direction = !window.mode.isDesktop() && L.DomUtil.isPortrait() ? 'x' : 'y';
 		this._scrollY = 0;
 
 		map.on('updateparts', this._updateDisabled, this);
@@ -123,7 +123,7 @@ L.Control.PartsPreview = L.Control.extend({
 				this._addDnDHandlers(frame);
 				frame.setAttribute('draggable', false);
 
-				if (!window.mode.isMobile()) {
+				if (window.mode.isDesktop()) {
 					L.DomUtil.setStyle(frame, 'height', '20px');
 					L.DomUtil.setStyle(frame, 'margin', '0em');
 				}
@@ -206,7 +206,7 @@ L.Control.PartsPreview = L.Control.extend({
 		img.hash = hashCode;
 		img.src = L.Icon.Default.imagePath + '/preview_placeholder.png';
 		img.fetched = false;
-		if (window.mode.isMobile() || window.mode.isTablet()) {
+		if (!window.mode.isDesktop()) {
 			(new Hammer(img, {recognizers: [[Hammer.Press]]}))
 			.on('press', L.bind(function () {
 				if (this._map._permission === 'edit') {
@@ -274,7 +274,7 @@ L.Control.PartsPreview = L.Control.extend({
 			var previewImgMinWidth = Math.round(parseFloat(L.DomUtil.getStyle(img, 'min-width')));
 			var imgHeight = imgSize.height;
 			var imgWidth = imgSize.width;
-			if (imgSize.width < previewImgMinWidth)
+			if (imgSize.width < previewImgMinWidth && window.mode.isDesktop())
 				imgHeight = Math.round(imgHeight * previewImgMinWidth / imgSize.width);
 			var previewFrameBB = img.parentElement.getBoundingClientRect();
 			if (this._direction === 'x') {
