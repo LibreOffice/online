c54c0ec210beaa888a513184d0140c59bbfccc76
gokaysatir <gokaysatir@collabora.com>
2020-06-11 17:12:57 +0300

    leaflet: Add php server unit test.
    Change-Id: I257c3cab95d646e5ff0a1356485a23ee997637c5

---
+++ test/UnitPHPServer.cpp
@@ -0,0 +1,39 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <memory>
+#include <ostream>
+#include <set>
+#include <string>
+
+#include <Poco/Exception.h>
+#include <Poco/RegularExpression.h>
+#include <Poco/URI.h>
+#include <test/lokassert.hpp>
+
+#include <Png.hpp>
+#include <Unit.hpp>
+#include <helpers.hpp>
+
+class LOOLWebSocket;
+
+
+/// Test suite for Calc.
+class UnitPHPServer : public UnitWSD
+{
+
+
+public:
+    void invokeTest() override;
+};
+
+
+UnitBase* unit_create_wsd(void) { return new UnitPHPServer(); }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
