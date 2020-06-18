LibreOffice/online

commit e95413d151c3f0d9476063c8520dd477342ed235

killpoco: removed StringTokenizer from common directory
removed use of Poco::StringTokenizer from the common directory
used LOOLProtocol::tokenize and std::vecor<std::string>

regex is used in Authorization.cpp due to limitation of toeknize mathod
regular expression helps to keep the original intention of the code

Change-Id: Ic87597d8b30cb385000f983389a57dc5d2533d98
Reviewed-on: https://gerrit.libreoffice.org/82575
Reviewed-by: Jan Holesovsky <kendy@collabora.com>
Tested-by: Jan Holesovsky <kendy@collabora.com>

common/Authorization.cpp
@@ -10,11 +10,11 @@
 #include <config.h>
 
 #include "Authorization.hpp"
+#include "Protocol.hpp"
 
 #include <cstdlib>
 #include <cassert>
-
-#include <Poco/StringTokenizer.h>
+#include <regex>
 
 void Authorization::authorizeURI(Poco::URI& uri) const
 {
@@ -50,19 +50,28 @@ void Authorization::authorizeRequest(Poco::Net::HTTPRequest& request) const
             // there might be more headers in here; like
             //   Authorization: Basic ....
             //   X-Something-Custom: Huh
-            Poco::StringTokenizer tokens(_data, "\n\r", Poco::StringTokenizer::TOK_IGNORE_EMPTY | Poco::StringTokenizer::TOK_TRIM);
+            // Regular expression evaluates and finds "\n\r" and tokenizes accordingly
+            std::vector<std::string> tokens(LOOLProtocol::tokenize(_data, std::regex(R"(\n\r)"), /*skipEmpty =*/ true));
             for (const auto& token : tokens)
             {
-                size_t i = token.find_first_of(':');
-                if (i != std::string::npos)
+                size_t separator = token.find_first_of(':');
+                if (separator != std::string::npos)
                 {
-                    size_t separator = i;
-                    for (++i; i < token.length() && token[i] == ' ';)
-                        ++i;
+                    size_t headerStart = token.find_first_not_of(' ', 0);
+                    size_t headerEnd = token.find_last_not_of(' ', separator - 1);
+
+                    size_t valueStart = token.find_first_not_of(' ', separator + 1);
+                    size_t valueEnd = token.find_last_not_of(' ');
 
                     // set the header
-                    if (i < token.length())
-                        request.set(token.substr(0, separator), token.substr(i));
+                    if (headerStart != std::string::npos && headerEnd != std::string::npos &&
+                            valueStart != std::string::npos && valueEnd != std::string::npos)
+                    {
+                        size_t headerLength = headerEnd - headerStart + 1;
+                        size_t valueLength = valueEnd - valueStart + 1;
+
+                        request.set(token.substr(headerStart, headerLength), token.substr(valueStart, valueLength));
+                    }
                 }
             }
             break;

common/IoUtil.cpp
@@ -23,7 +23,6 @@
 
 #include <Poco/Net/NetException.h>
 #include <Poco/Net/Socket.h>
-#include <Poco/StringTokenizer.h>
 #include <Poco/Thread.h>
 #include <Poco/URI.h>
 

common/Session.hpp
@@ -19,7 +19,6 @@
 #include <Poco/Buffer.h>
 #include <Poco/Path.h>
 #include <Poco/Process.h>
-#include <Poco/StringTokenizer.h>
 #include <Poco/Types.h>
 
 #include "Protocol.hpp"

commit d66c96c98b2459403111ba3038463a4e96ceb071

Sanitize the access_header.
The access_header can contain a lot of nonsense, like whitespace around
or additional \n's or \r's.  We used to sanitize that, but then
regressed in e95413d151c3f0d9476063c8520dd477342ed235 where the
"tokenize by any of \n\r" was by mistake replaced with "tokenize by
string '\n\r'".

Unfortunately the unit test didn't uncover that, and the further
refactorings of the related code have hidden that even more.

Change-Id: Ie2bf950d0426292770b599e40ee2401101162ff2

common/Authorization.cpp
@@ -52,8 +52,9 @@ void Authorization::authorizeRequest(Poco::Net::HTTPRequest& request) const
             // there might be more headers in here; like
             //   Authorization: Basic ....
             //   X-Something-Custom: Huh
-            // Regular expression evaluates and finds "\n\r" and tokenizes accordingly
-            StringVector tokens(Util::tokenize(_data, "\n\r"));
+            // Split based on \n's or \r's and trim, to avoid nonsense in the
+            // headers
+            StringVector tokens(Util::tokenizeAnyOf(_data, "\n\r"));
             for (auto it = tokens.begin(); it != tokens.end(); ++it)
             {
                 std::string token = tokens.getParam(*it);

common/Util.cpp
@@ -987,6 +987,51 @@ namespace Util
         }
     #endif
 
+    StringVector tokenizeAnyOf(const std::string& s, const char* delimiters)
+    {
+        // trim from the end so that we do not have to check this exact case
+        // later
+        std::size_t length = s.length();
+        while (length > 0 && s[length - 1] == ' ')
+            --length;
+
+        if (length == 0)
+            return StringVector();
+
+        std::size_t delimitersLength = std::strlen(delimiters);
+        std::size_t start = 0;
+
+        std::vector<StringToken> tokens;
+        tokens.reserve(16);
+
+        while (start < length)
+        {
+            // ignore the leading whitespace
+            while (start < length && s[start] == ' ')
+                ++start;
+
+            // anything left?
+            if (start == length)
+                break;
+
+            std::size_t end = s.find_first_of(delimiters, start, delimitersLength);
+            if (end == std::string::npos)
+                end = length;
+
+            // trim the trailing whitespace
+            std::size_t trimEnd = end;
+            while (start < trimEnd && s[trimEnd - 1] == ' ')
+                --trimEnd;
+
+            // add only non-empty tokens
+            if (start < trimEnd)
+                tokens.emplace_back(start, trimEnd - start);
+
+            start = end + 1;
+        }
+
+        return StringVector(s, std::move(tokens));
+    }
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

common/Util.hpp
@@ -433,29 +433,12 @@ namespace Util
         return StringVector(s, std::move(tokens));
     }
 
-    inline StringVector tokenize(const std::string& s, const char* delimiter)
-    {
-        if (s.empty())
-            return StringVector();
+    /** Tokenize based on any of the characters in 'delimiters'.
 
-        std::size_t start = 0;
-        std::size_t end = s.find(delimiter, start);
-
-        std::vector<StringToken> tokens;
-        tokens.reserve(16);
-
-        tokens.emplace_back(start, end - start);
-        start = end + std::strlen(delimiter);
-
-        while (end != std::string::npos)
-        {
-            end = s.find(delimiter, start);
-            tokens.emplace_back(start, end - start);
-            start = end + std::strlen(delimiter);
-        }
-
-        return StringVector(s, std::move(tokens));
-    }
+        Ie. when there is '\n\r' in there, any of them means a delimiter.
+        In addition, trim the values so there are no leadiding or trailing spaces.
+    */
+    StringVector tokenizeAnyOf(const std::string& s, const char* delimiters);
 
 #ifdef IOS
 

test/WhiteBoxTests.cpp
@@ -33,6 +33,7 @@ class WhiteBoxTests : public CPPUNIT_NS::TestFixture
     CPPUNIT_TEST(testSplitting);
     CPPUNIT_TEST(testMessageAbbreviation);
     CPPUNIT_TEST(testTokenizer);
+    CPPUNIT_TEST(testTokenizerTokenizeAnyOf);
     CPPUNIT_TEST(testReplace);
     CPPUNIT_TEST(testRegexListMatcher);
     CPPUNIT_TEST(testRegexListMatcher_Init);
@@ -54,6 +55,7 @@ class WhiteBoxTests : public CPPUNIT_NS::TestFixture
     void testSplitting();
     void testMessageAbbreviation();
     void testTokenizer();
+    void testTokenizerTokenizeAnyOf();
     void testReplace();
     void testRegexListMatcher();
     void testRegexListMatcher_Init();
@@ -424,6 +426,67 @@ void WhiteBoxTests::testTokenizer()
 
     ints = LOOLProtocol::tokenizeInts(std::string(",,,"));
     LOK_ASSERT_EQUAL(static_cast<size_t>(0), ints.size());
+}
+
+void WhiteBoxTests::testTokenizerTokenizeAnyOf()
+{
+    StringVector tokens;
+    const char delimiters[] = "\n\r"; // any of these delimits; and we trim whitespace
+
+    tokens = Util::tokenizeAnyOf("", delimiters);
+    LOK_ASSERT_EQUAL(static_cast<size_t>(0), tokens.size());
+
+    tokens = Util::tokenizeAnyOf("  ", delimiters);
+    LOK_ASSERT_EQUAL(static_cast<size_t>(0), tokens.size());
+
+    tokens = Util::tokenizeAnyOf("A", delimiters);
+    LOK_ASSERT_EQUAL(static_cast<size_t>(1), tokens.size());
+    LOK_ASSERT_EQUAL(std::string("A"), tokens[0]);
+
+    tokens = Util::tokenizeAnyOf("  A", delimiters);
+    LOK_ASSERT_EQUAL(static_cast<size_t>(1), tokens.size());
+    LOK_ASSERT_EQUAL(std::string("A"), tokens[0]);
+
+    tokens = Util::tokenizeAnyOf("A  ", delimiters);
+    LOK_ASSERT_EQUAL(static_cast<size_t>(1), tokens.size());
+    LOK_ASSERT_EQUAL(std::string("A"), tokens[0]);
+
+    tokens = Util::tokenizeAnyOf(" A ", delimiters);
+    LOK_ASSERT_EQUAL(static_cast<size_t>(1), tokens.size());
+    LOK_ASSERT_EQUAL(std::string("A"), tokens[0]);
+
+    tokens = Util::tokenizeAnyOf(" A  Z ", delimiters);
+    LOK_ASSERT_EQUAL(static_cast<size_t>(1), tokens.size());
+    LOK_ASSERT_EQUAL(std::string("A  Z"), tokens[0]);
+
+    tokens = Util::tokenizeAnyOf("\n", delimiters);
+    LOK_ASSERT_EQUAL(static_cast<size_t>(0), tokens.size());
+
+    tokens = Util::tokenizeAnyOf("\n\r\r\n", delimiters);
+    LOK_ASSERT_EQUAL(static_cast<size_t>(0), tokens.size());
+
+    tokens = Util::tokenizeAnyOf(" A  \nZ ", delimiters);
+    LOK_ASSERT_EQUAL(static_cast<size_t>(2), tokens.size());
+    LOK_ASSERT_EQUAL(std::string("A"), tokens[0]);
+    LOK_ASSERT_EQUAL(std::string("Z"), tokens[1]);
+
+    tokens = Util::tokenizeAnyOf(" A  Z\n ", delimiters);
+    LOK_ASSERT_EQUAL(static_cast<size_t>(1), tokens.size());
+    LOK_ASSERT_EQUAL(std::string("A  Z"), tokens[0]);
+
+    tokens = Util::tokenizeAnyOf(" A  Z  \n\r\r\n ", delimiters);
+    LOK_ASSERT_EQUAL(static_cast<size_t>(1), tokens.size());
+    LOK_ASSERT_EQUAL(std::string("A  Z"), tokens[0]);
+
+    tokens = Util::tokenizeAnyOf(" A  \n\r\r\n  \r  \n  Z  \n ", delimiters);
+    LOK_ASSERT_EQUAL(static_cast<size_t>(2), tokens.size());
+    LOK_ASSERT_EQUAL(std::string("A"), tokens[0]);
+    LOK_ASSERT_EQUAL(std::string("Z"), tokens[1]);
+
+    tokens = Util::tokenizeAnyOf("  \r A  \n  \r  \n  Z  \n ", delimiters);
+    LOK_ASSERT_EQUAL(static_cast<size_t>(2), tokens.size());
+    LOK_ASSERT_EQUAL(std::string("A"), tokens[0]);
+    LOK_ASSERT_EQUAL(std::string("Z"), tokens[1]);
 }
 
 void WhiteBoxTests::testReplace()