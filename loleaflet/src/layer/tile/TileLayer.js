5bf2c8d6d613b0a43cb69e97b76b450ba4b06a20
Aron Budea <aron.budea@collabora.com>
2020-06-25 09:25:53 +0200

    loleaflet: use correct extension for default user avatar
    Regression from 8bfa1318b246d7c9df2b1d22d2e8094205a53acd
    
    Change-Id: I8be2ea394fd35c418b33aed42555cfa37d7bc7c9

--- loleaflet/src/layer/tile/TileLayer.js
+++ loleaflet/src/layer/tile/TileLayer.js
@@ -432,7 +432,7 @@ L.TileLayer = L.GridLayer.extend({
 		var tdImg = L.DomUtil.create(tagTd, 'loleaflet-annotation-img', tr);
 		var tdAuthor = L.DomUtil.create(tagTd, 'loleaflet-annotation-author', tr);
 		var imgAuthor = L.DomUtil.create('img', 'avatar-img', tdImg);
-		imgAuthor.setAttribute('src', L.LOUtil.getImageURL('user.svg'));
+		imgAuthor.setAttribute('src', L.LOUtil.getImageURL('user.png'));
 		imgAuthor.setAttribute('width', 32);
 		imgAuthor.setAttribute('height', 32);
 		this._authorAvatarImg = imgAuthor;
