LibreOffice/online

commit a59076e1ca88a6f2c8c67ce45bf769e963cf0717

add split-panes support for overlay layer
(This is only for svg renderer)

There are separate svg DOM-nodes for each split-pane with view-box set
appropriately. The L.Path based objects are shared for each
split-pane, but there will be separate identical 'path' DOM-nodes for
each svg container.

This patch introduces L.SplitPanesRenderer/L.SplitPanesSVG (has
same external api as L.Renderer/L.SVG). These are wrapper classes to host
child renderers, one per split-pane and delegate calls to them
appropriately.

Change-Id: Id44e9a1312500e6b43cdd8e4f42e235b43d22772
Reviewed-on: https://gerrit.libreoffice.org/c/online/+/98354
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Tested-by: Jenkins
Reviewed-by: Dennis Francis <dennis.francis@collabora.com>

loleaflet/src/layer/vector/SVGGroup.js
@@ -12,8 +12,10 @@ L.SVGGroup = L.Layer.extend({
 
 	initialize: function (bounds, options) {
 		L.setOptions(this, options);
+		this._pathNodeCollection = new L.Path.PathNodeCollection();
 		this._bounds = bounds;
 		this._rect = L.rectangle(bounds, this.options);
+		this._hasSVGNode = false;
 		if (L.Browser.touch && !L.Browser.pointer) {
 			this.options.manualDrag = true;
 		}
@@ -23,20 +25,28 @@ L.SVGGroup = L.Layer.extend({
 	},
 
 	setVisible: function (visible) {
-		if (this._svg != null) {
+		this._forEachSVGNode(function (svgNode) {
+			svgNode.setAttribute('opacity', 0);
 			if (visible)
-				this._svg.setAttribute('visibility', 'visible');
+				svgNode.setAttribute('visibility', 'visible');
 			else
-				this._svg.setAttribute('visibility', 'hidden');
-		}
+				svgNode.setAttribute('visibility', 'hidden');
+		});
 	},
 
 	sizeSVG: function () {
+
+		if (!this._hasSVGNode) {
+			return;
+		}
+
 		var size = L.bounds(this._map.latLngToLayerPoint(this._bounds.getNorthWest()),
 			this._map.latLngToLayerPoint(this._bounds.getSouthEast())).getSize();
 
-		this._svg.setAttribute('width', size.x);
-		this._svg.setAttribute('height', size.y);
+		this._forEachSVGNode(function (svgNode) {
+			svgNode.setAttribute('width', size.x);
+			svgNode.setAttribute('height', size.y);
+		});
 	},
 
 	parseSVG: function (svgString) {
@@ -45,30 +55,41 @@ L.SVGGroup = L.Layer.extend({
 	},
 
 	addEmbeddedSVG: function (svgString) {
-		var doc = this.parseSVG(svgString);
+		var svgDoc = this.parseSVG(svgString);
 
-		if (doc.lastChild.localName !== 'svg')
+		if (svgDoc.lastChild.localName !== 'svg')
 			return;
 
-		this._svg = this._path.insertBefore(doc.lastChild, this._rect._path);
-		this._dragShape = this._rect._path;
-		this._svg.setAttribute('pointer-events', 'none');
-		this._svg.setAttribute('opacity', this._dragStarted ? 1 : 0);
+		var svgLastChild = svgDoc.lastChild;
+		var thisObj = this;
+		this._forEachGroupNode(function (groupNode, rectNode, nodeData) {
+			var svgNode = groupNode.insertBefore(svgLastChild, rectNode);
+			nodeData.setCustomField('svg', svgNode);
+			nodeData.setCustomField('dragShape', rectNode);
+			thisObj._dragShapePresent = true;
+			svgNode.setAttribute('pointer-events', 'none');
+			svgNode.setAttribute('opacity', thisObj._dragStarted ? 1 : 0);
+		});
+
+		this._hasSVGNode = true;
+
 		this.sizeSVG();
 		this._update();
 	},
 
 	_onDragStart: function(evt) {
-		if (!this._map || !this._dragShape || !this.dragging)
+		if (!this._map || !this._dragShapePresent || !this.dragging)
 			return;
 		this._dragStarted = true;
 		this._moved = false;
 
 		if (!this.options.manualDrag) {
-			L.DomEvent.on(this._dragShape, 'mousemove', this._onDrag, this);
-			L.DomEvent.on(this._dragShape, 'mouseup', this._onDragEnd, this);
-			if (this.dragging.constraint)
-				L.DomEvent.on(this._dragShape, 'mouseout', this._onDragEnd, this);
+			this._forEachDragShape(function (dragShape) {
+				L.DomEvent.on(dragShape, 'mousemove', this._onDrag, this);
+				L.DomEvent.on(dragShape, 'mouseup', this._onDragEnd, this);
+				if (this.dragging.constraint)
+					L.DomEvent.on(dragShape, 'mouseout', this._onDragEnd, this);
+			}.bind(this));
 		}
 
 		var data = {
@@ -82,7 +103,7 @@ L.SVGGroup = L.Layer.extend({
 	},
 
 	_onDrag: function(evt) {
-		if (!this._map || !this._dragShape || !this.dragging)
+		if (!this._map || !this._dragShapePresent || !this.dragging)
 			return;
 
 		if (!this._moved) {
@@ -94,14 +115,16 @@ L.SVGGroup = L.Layer.extend({
 	},
 
 	_onDragEnd: function(evt) {
-		if (!this._map || !this._dragShape || !this.dragging)
+		if (!this._map || !this._dragShapePresent || !this.dragging)
 			return;
 
 		if (!this.options.manualDrag) {
-			L.DomEvent.off(this._dragShape, 'mousemove', this._onDrag, this);
-			L.DomEvent.off(this._dragShape, 'mouseup', this._onDragEnd, this);
-			if (this.dragging.constraint)
-				L.DomEvent.off(this._dragShape, 'mouseout', this._onDragEnd, this);
+			this._forEachDragShape(function (dragShape) {
+				L.DomEvent.off(dragShape, 'mousemove', this._onDrag, this);
+				L.DomEvent.off(dragShape, 'mouseup', this._onDragEnd, this);
+				if (this.dragging.constraint)
+					L.DomEvent.off(dragShape, 'mouseout', this._onDragEnd, this);
+			}.bind(this));
 		}
 
 		this._moved = false;
@@ -146,53 +169,78 @@ L.SVGGroup = L.Layer.extend({
 		this._renderer._initPath(this._rect);
 		this._renderer._addGroup(this);
 
-		if (this._path && this._rect._path) {
+		this._forEachGroupNode(function (groupNode, rectNode, nodeData) {
+
+			if (!groupNode || !rectNode) {
+				return;
+			}
+
 			this._rect._map = this._map;
 			this._rect._renderer = this._renderer;
-			L.DomUtil.addClass(this._path, 'leaflet-control-buttons-disabled');
+			L.DomUtil.addClass(groupNode, 'leaflet-control-buttons-disabled');
 
 			if (this.options.svg) {
 				var doc = this.parseSVG(this.options.svg);
 				if (doc && doc.lastChild.localName === 'svg') {
-					this._svg = this._path.appendChild(doc.lastChild);
-					this._svg.setAttribute('opacity', 0);
-					this._svg.setAttribute('pointer-events', 'none');
-					this.sizeSVG();
+					this._hasSVGNode = true;
+					var svgNode = groupNode.appendChild(doc.lastChild);
+					nodeData.setCustomField('svg', svgNode);
+					svgNode.setAttribute('opacity', 0);
+					svgNode.setAttribute('pointer-events', 'none');
 				}
 				delete this.options.svg;
 			}
 
-			this._path.appendChild(this._rect._path);
-			this._dragShape = this._rect._path;
+			groupNode.appendChild(rectNode);
+			nodeData.setCustomField('dragShape', rectNode);
+			this._dragShapePresent = true;
 
 			if (!this.options.manualDrag) {
-				L.DomEvent.on(this._rect._path, 'mousedown', this._onDragStart, this);
+				L.DomEvent.on(rectNode, 'mousedown', this._onDragStart, this);
 			}
-		}
+		}.bind(this));
+
+		this.sizeSVG();
+
 		this._update();
 	},
 
 	onRemove: function () {
 		this._rect._map = this._rect._renderer = null;
-		this.removeInteractiveTarget(this._rect._path);
-		L.DomUtil.remove(this._rect._path);
+		this._pathNodeCollection.forEachNode(function (nodeData) {
+
+			var actualRenderer = nodeData.getActualRenderer();
+			var rectNode = this._rect.getPathNode(actualRenderer);
+
+			this.removeInteractiveTarget(rectNode);
+			L.DomUtil.remove(rectNode);
+
+		}.bind(this));
+
 		this.removeEmbeddedSVG();
 		this._renderer._removeGroup(this);
 	},
 
 	removeEmbeddedSVG: function () {
-		if (this._svg) {
-			this._dragShape = null;
-			L.DomUtil.remove(this._svg);
-			delete this._svg;
-			this._update();
+		if (!this._hasSVGNode) {
+			return;
 		}
+
+		this._pathNodeCollection.forEachNode(function (nodeData) {
+			var svgNode = nodeData.getCustomField('svg');
+			L.DomUtil.remove(svgNode);
+			nodeData.clearCustomField('svg');
+		});
+
+		this._dragShapePresent = false;
+		this._hasSVGNode = false;
+		this._update();
 	},
 
 	_hideEmbeddedSVG: function () {
-		if (this._svg) {
-			this._svg.setAttribute('opacity', 0);
-		}
+		this._forEachSVGNode(function (svgNode) {
+			svgNode.setAttribute('opacity', 0);
+		});
 	},
 
 	_transform: function(matrix) {
@@ -216,23 +264,95 @@ L.SVGGroup = L.Layer.extend({
 	},
 
 	_showEmbeddedSVG: function () {
-		if (this._svg) {
-			this._svg.setAttribute('opacity', 1);
-		}
+		this._forEachSVGNode(function (svgNode) {
+			svgNode.setAttribute('opacity', 1);
+		});
 	},
 
 	_update: function () {
 		this._rect.setBounds(this._bounds);
-		if (this._svg) {
-			var point = this._map.latLngToLayerPoint(this._bounds.getNorthWest());
-			this._svg.setAttribute('x', point.x);
-			this._svg.setAttribute('y', point.y);
-		}
+		var point = this._map.latLngToLayerPoint(this._bounds.getNorthWest());
+
+		this._forEachSVGNode(function (svgNode) {
+			svgNode.setAttribute('x', point.x);
+			svgNode.setAttribute('y', point.y);
+		}.bind(this));
 	},
 
 	_updatePath: function () {
 		this._update();
-	}
+	},
+
+	addPathNode: function (pathNode, actualRenderer) {
+
+		this._path = undefined;
+
+		if (!this._pathNodeCollection) {
+			this._pathNodeCollection = new L.Path.PathNodeCollection();
+		}
+
+		this._pathNodeCollection.add(new L.Path.PathNodeData(pathNode, actualRenderer));
+	},
+
+	getPathNode: function (actualRenderer) {
+
+		console.assert(this._pathNodeCollection, 'missing _pathNodeCollection member!');
+		return this._pathNodeCollection.getPathNode(actualRenderer);
+	},
+
+	addClass: function (className) {
+		this._pathNodeCollection.addOrRemoveClass(className, true /* add */);
+	},
+
+	removeClass: function (className) {
+		this._pathNodeCollection.addOrRemoveClass(className, false /* add */);
+	},
+
+	_forEachGroupNode: function (callback) {
+
+		var that = this;
+		this._pathNodeCollection.forEachNode(function (nodeData) {
+
+			var actualRenderer = nodeData.getActualRenderer();
+			var groupNode = nodeData.getNode();
+			var rectNode = that._rect.getPathNode(actualRenderer);
+
+			callback(groupNode, rectNode, nodeData);
+
+		});
+
+		return true;
+	},
+
+	_forEachSVGNode: function (callback) {
+		if (!this._hasSVGNode) {
+			return false;
+		}
+
+		this._pathNodeCollection.forEachNode(function (nodeData) {
+			var svgNode = nodeData.getCustomField('svg');
+			if (svgNode) {
+				callback(svgNode);
+			}
+		});
+
+		return true;
+	},
+
+	_forEachDragShape: function (callback) {
+		if (!this._dragShapePresent) {
+			return false;
+		}
+
+		this._pathNodeCollection.forEachNode(function (nodeData) {
+			var dragShape = nodeData.getCustomField('dragShape');
+			if (dragShape) {
+				callback(dragShape);
+			}
+		});
+
+		return true;
+	},
 
 });
 

loleaflet/Makefile.am
@@ -234,6 +234,8 @@ LOLEAFLET_JS =\
 	src/layer/vector/CircleMarker.js \
 	src/layer/vector/Circle.js \
 	src/layer/vector/SVG.js \
+	src/layer/vector/SplitPanesRenderer.js \
+	src/layer/vector/SplitPanesSVG.js \
 	src/layer/vector/Path.Transform.SVG.js \
 	src/core/Handler.js \
 	src/layer/vector/SVGGroup.js \

loleaflet/src/layer/vector/CircleMarker.js
@@ -66,7 +66,7 @@ L.CircleMarker = L.Path.extend({
 	},
 
 	_empty: function () {
-		return this._radius && !this._renderer._bounds.intersects(this._pxBounds);
+		return this._radius && !this._renderer.intersectsBounds(this._pxBounds);
 	}
 });
 

loleaflet/src/layer/vector/Path.Drag.js
@@ -72,9 +72,7 @@ L.Handler.PathDrag = L.Handler.extend(/** @lends  L.Path.Drag.prototype */ {
 			(this._path.options.className + ' ' + L.Handler.PathDrag.DRAGGING_CLS) :
 			 L.Handler.PathDrag.DRAGGING_CLS;
 
-		if (this._path._path) {
-			L.DomUtil.addClass(this._path._path, L.Handler.PathDrag.DRAGGING_CLS);
-		}
+		this._path.addClass(L.Handler.PathDrag.DRAGGING_CLS);
 	},
 
 	/**
@@ -85,9 +83,8 @@ L.Handler.PathDrag = L.Handler.extend(/** @lends  L.Path.Drag.prototype */ {
 
 		this._path.options.className = this._path.options.className
 			.replace(new RegExp('\\s+' + L.Handler.PathDrag.DRAGGING_CLS), '');
-		if (this._path._path) {
-			L.DomUtil.removeClass(this._path._path, L.Handler.PathDrag.DRAGGING_CLS);
-		}
+
+		this._path.removeClass(L.Handler.PathDrag.DRAGGING_CLS);
 
 		if (!this._path.options.manualDrag) {
 			L.DomEvent.off(document, 'mousemove touchmove', this._onDrag,    this);
@@ -119,7 +116,7 @@ L.Handler.PathDrag = L.Handler.extend(/** @lends  L.Path.Drag.prototype */ {
 		this._matrix = [1, 0, 0, 1, 0, 0];
 		L.DomEvent.stop(evt.originalEvent);
 
-		L.DomUtil.addClass(this._path._renderer._container, 'leaflet-interactive');
+		this._path._renderer.addContainerClass('leaflet-interactive');
 
 		if (!this._path.options.manualDrag) {
 			L.DomEvent

loleaflet/src/layer/vector/Path.Transform.SVG.js
@@ -4,7 +4,7 @@ L.SVG.include({
 	 * Reset transform matrix
 	 */
 	_resetTransformPath: function(layer) {
-		layer._path.setAttributeNS(null, 'transform', '');
+		layer.getPathNode(this).setAttributeNS(null, 'transform', '');
 	},
 
 	/**
@@ -13,8 +13,40 @@ L.SVG.include({
 	 * @param {Array.<Number>} matrix
 	 */
 	transformPath: function(layer, matrix) {
-		layer._path.setAttributeNS(null, 'transform',
+		layer.getPathNode(this).setAttributeNS(null, 'transform',
 			'matrix(' + matrix.join(' ') + ')');
 	}
 
+});
+
+L.SplitPanesSVG.include({
+	/**
+	 * Reset transform matrix
+	 */
+	_resetTransformPath: function(layer) {
+		if (layer.options.fixed === true) {
+			this._childRenderers['fixed']._resetTransformPath(layer);
+			return;
+		}
+
+		this._forEachPaneRenderer(function (paneRenderer) {
+			paneRenderer._resetTransformPath(layer);
+		});
+	},
+
+	/**
+	 * Applies matrix transformation to SVG
+	 * @param {L.Path}         layer
+	 * @param {Array.<Number>} matrix
+	 */
+	transformPath: function(layer, matrix) {
+		if (layer.options.fixed === true) {
+			this._childRenderers['fixed'].transformPath(layer, matrix);
+			return;
+		}
+
+		this._forEachPaneRenderer(function (paneRenderer) {
+			paneRenderer.transformPath(layer, matrix);
+		});
+	}
 });

loleaflet/src/layer/vector/Path.js
@@ -21,10 +21,12 @@ L.Path = L.Layer.extend({
 		fillRule: 'evenodd',
 
 		// className: ''
-		interactive: true
+		interactive: true,
+		fixed: false,
 	},
 
 	onAdd: function () {
+		this._pathNodeCollection = new L.Path.PathNodeCollection();
 		this._renderer = this._map.getRenderer(this);
 		this._renderer._initPath(this);
 		this._reset();
@@ -33,6 +35,7 @@ L.Path = L.Layer.extend({
 
 	onRemove: function () {
 		this._renderer._removePath(this);
+		this._pathNodeCollection.clear();
 	},
 
 	getEvents: function () {
@@ -80,5 +83,135 @@ L.Path = L.Layer.extend({
 	_clickTolerance: function () {
 		// used when doing hit detection for Canvas layers
 		return (this.options.stroke ? this.options.weight / 2 : 0) + (L.Browser.touch ? 10 : 0);
-	}
+	},
+
+	addPathNode: function (pathNode, actualRenderer) {
+
+		this._path = undefined;
+
+		if (!this._pathNodeCollection) {
+			this._pathNodeCollection = new L.Path.PathNodeCollection();
+		}
+
+		this._pathNodeCollection.add(new L.Path.PathNodeData(pathNode, actualRenderer));
+	},
+
+	getPathNode: function (actualRenderer) {
+		return this._pathNodeCollection.getPathNode(actualRenderer);
+	},
+
+	addClass: function (className) {
+		this._pathNodeCollection.addOrRemoveClass(className, true /* add */);
+	},
+
+	removeClass: function (className) {
+		this._pathNodeCollection.addOrRemoveClass(className, false /* add */);
+	},
+
+});
+
+L.Path.PathNodeData = L.Class.extend({
+
+	initialize: function (pathNode, actualRenderer) {
+
+		console.assert(pathNode, 'invalid pathNode argument!');
+		console.assert(actualRenderer, 'invalid actualRenderer argument!');
+
+		if (!(pathNode instanceof Node)) {
+			console.error('Not a node instance!');
+		}
+
+		this._pathNode = pathNode;
+		this._actualRenderer = actualRenderer;
+		this._data = {};
+	},
+
+	key: function () {
+		return L.Path.PathNodeData.key(this._actualRenderer);
+	},
+
+	getNode: function () {
+		if (!(this._pathNode instanceof Node)) {
+			console.error('Not a node instance!');
+		}
+		return this._pathNode;
+	},
+
+	getActualRenderer: function () {
+		return this._actualRenderer;
+	},
+
+	setCustomField: function (fieldName, value) {
+		console.assert(typeof fieldName === 'string' && fieldName, 'invalid fieldName');
+		this._data[fieldName] = value;
+	},
+
+	getCustomField: function (fieldName) {
+		console.assert(typeof fieldName === 'string' && fieldName, 'invalid fieldName');
+		return this._data[fieldName];
+	},
+
+	clearCustomField: function (fieldName) {
+		console.assert(typeof fieldName === 'string' && fieldName, 'invalid fieldName');
+		delete this._data[fieldName];
+	},
+
+	addOrRemoveClass: function (className, add) {
+		if (add) {
+			L.DomUtil.addClass(this._pathNode, className);
+		}
+		else {
+			L.DomUtil.removeClass(this._pathNode, className);
+		}
+	},
+
+});
+
+L.Path.PathNodeData.key = function (layer) {
+	return L.stamp(layer);
+};
+
+L.Path.PathNodeCollection = L.Class.extend({
+
+	initialize: function () {
+		this.clear();
+	},
+
+	add: function (pathNodeData) {
+
+		console.assert(pathNodeData instanceof L.Path.PathNodeData,
+			'invalid pathNodeData argument!');
+
+		this._collection[pathNodeData.key()] = pathNodeData;
+	},
+
+	clear: function () {
+		this._collection = {};
+	},
+
+	getPathNode: function (actualRenderer) {
+
+		console.assert(actualRenderer, 'invalid actualRenderer argument!');
+		var key = L.Path.PathNodeData.key(actualRenderer);
+		var nodeData = this._collection[key];
+
+		console.assert(nodeData, 'cannot find path node!');
+
+		return nodeData.getNode();
+	},
+
+	forEachNode: function (callback) {
+		var that = this;
+		Object.keys(this._collection).forEach(function (key) {
+			callback(that._collection[key]);
+		});
+	},
+
+	addOrRemoveClass: function (className, add) {
+		console.assert(className, 'className not provided!');
+		this.forEachNode(function (nodeData) {
+			nodeData.addOrRemoveClass(className, add);
+		});
+	},
+
 });

loleaflet/src/layer/vector/Polygon.js
@@ -42,7 +42,9 @@ L.Polygon = L.Polyline.extend({
 	},
 
 	_clipPoints: function () {
-		if (this.options.noClip) {
+		if (this.options.noClip || this._renderer instanceof L.SplitPanesSVG) {
+			// TODO: need some work to get this right and performant, especially in the case of
+			// a poly* spread across multiple split-panes.
 			this._parts = this._rings;
 			return;
 		}

loleaflet/src/layer/vector/Polyline.js
@@ -165,7 +165,7 @@ L.Polyline = L.Path.extend({
 
 	// clip polyline by renderer bounds so that we have less to render for performance
 	_clipPoints: function () {
-		if (this.options.noClip) {
+		if (this.options.noClip || this._renderer instanceof L.SplitPanesSVG) {
 			this._parts = this._rings;
 			return;
 		}

commit a7e72adde98cbae34fdb4d93b84f34c65ac94301

svggroup: remove incorrect setting of opacity
This was introduced in a59076e1ca88a6f2c8c67ce45bf769e963cf0717

Change-Id: I79b3281c41b16afc73625cc48b2996e2438209e3

loleaflet/src/layer/vector/SVGGroup.js
@@ -26,7 +26,6 @@ L.SVGGroup = L.Layer.extend({
 
 	setVisible: function (visible) {
 		this._forEachSVGNode(function (svgNode) {
-			svgNode.setAttribute('opacity', 0);
 			if (visible)
 				svgNode.setAttribute('visibility', 'visible');
 			else