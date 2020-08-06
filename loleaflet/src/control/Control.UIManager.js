f626a3ca993c092ccdf456155776fb3f812b9753
Szymon Kłos <szymon.klos@collabora.com>
2020-08-06 12:32:55 +0200

    Handle back button on mobile
    This patch created states in history when using
    mobilewizard, allows to use back button which navigates
    back in the menu.
    
    Used History API and popstate event.
    
    Also unified onClose handler to close the app.
    
    Change-Id: Id9da9bf71ee6183525ea20d43a416d3ddabec7c2

--- loleaflet/src/control/Control.UIManager.js
+++ loleaflet/src/control/Control.UIManager.js
@@ -6,10 +6,13 @@
 
 /* global $ setupToolbar w2ui w2utils */
 L.Control.UIManager = L.Control.extend({
+	mobileWizard: null,
+
 	onAdd: function (map) {
 		this.map = map;
 
 		map.on('updatepermission', this.onUpdatePermission, this);
+		window.addEventListener('popstate', this.onGoBack.bind(this));
 	},
 
 	// UI initialization
@@ -37,7 +40,8 @@ L.Control.UIManager = L.Control.extend({
 		this.map.addControl(L.control.documentNameInput());
 		this.map.addControl(L.control.scroll());
 		this.map.addControl(L.control.alertDialog());
-		this.map.addControl(L.control.mobileWizard());
+		this.mobileWizard = L.control.mobileWizard();
+		this.map.addControl(this.mobileWizard);
 		this.map.addControl(L.control.languageDialog());
 		this.map.dialog = L.control.lokDialog();
 		this.map.addControl(this.map.dialog);
@@ -187,6 +191,19 @@ L.Control.UIManager = L.Control.extend({
 
 		// We've resized the document container.
 		this.map.invalidateSize();
+	},
+
+	onGoBack: function(popStateEvent) {
+		if (popStateEvent.state
+			&& popStateEvent.state.context
+			&& popStateEvent.state.context === 'mobile-wizard'
+			&& this.mobileWizard) {
+			if (this.mobileWizard.isOpen()) {
+				this.mobileWizard.goLevelUp(true);
+			} else {
+				window.onClose();
+			}
+		}
 	},
 
 	// Helper functions
