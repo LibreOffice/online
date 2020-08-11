211864e7727ec478f508a4ab1a1d6c0bb634b6e1
Dennis Francis <dennis.francis@collabora.com>
2020-08-11 17:21:51 +0530

    dontpush: run cypress tests for CanvasTileLayer based Calc
    Change-Id: Ib02c753e57595065bfbfa7e27dba67964be75178

--- loleaflet/src/layer/tile/CalcTileLayer.js
+++ loleaflet/src/layer/tile/CalcTileLayer.js
@@ -4,7 +4,7 @@
  */
 
 /* global */
-L.CalcTileLayer = (L.Browser.mobile ? L.TileLayer : L.CanvasTileLayer).extend({
+L.CalcTileLayer = ((window.mode.isMobile() || window.mode.isTablet()) && !L.Browser.cypressTest ? L.TileLayer : L.CanvasTileLayer).extend({
 	options: {
 		// TODO: sync these automatically from SAL_LOK_OPTIONS
 		sheetGeometryDataEnabled: true,
