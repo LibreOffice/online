b742258ad70b5ee226b7c44bb9d782abbc1376b4
Ezinne Nnamani <nnamani.ezinne@collabora.com>
2020-09-04 16:04:13 +0100

    A fix to display the avatar on the comments in Impress slides
    Change-Id: I1ba699d5e8583253ec9d14502629218d8714e5aa
    Reviewed-on: https://gerrit.libreoffice.org/c/online/+/102141
    Tested-by: Jenkins
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- loleaflet/src/layer/tile/ImpressTileLayer.js
+++ loleaflet/src/layer/tile/ImpressTileLayer.js
@@ -405,6 +405,9 @@ L.ImpressTileLayer = L.TileLayer.extend({
 	_onMessage: function (textMsg, img) {
 		if (textMsg.startsWith('comment:')) {
 			var obj = JSON.parse(textMsg.substring('comment:'.length + 1));
+			if (object.comment.author in this._map._viewInfoByUserName) {
+				object.comment.avatar = this._map._viewInfoByUserName[object.comment.author].userextrainfo.avatar;
+			}
 			if (obj.comment.action === 'Add') {
 				if (!this._annotations[obj.comment.parthash]) {
 					this._annotations[obj.comment.parthash] = [];
